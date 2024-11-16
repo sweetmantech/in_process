74249ba07c196314ca170792bfc69682cb2e6f5b
Sweets Sweetman <sweetmantech@gmail.com>
2024-11-16 20:02:12 +0700

    Add a token page route which uses the same page rendering as the collection page. /collect/[collection]/[tokenId]

---
+++ app/collect/[collection]/[tokenId]/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import FeedPage from "@/components/FeedPage";
+import { useParams } from "next/navigation";
+
+export default function TokenPage() {
+  const params = useParams();
+  const collection = params.collection as string;
+  const [chain, address] = collection.split("%3A");
+
+  return <FeedPage chain={chain} address={address} />;
+}
