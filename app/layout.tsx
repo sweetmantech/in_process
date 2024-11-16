b133ae9d17539cad497b1cbc3c4a4da97c0468d6
Sweets Sweetman <sweetmantech@gmail.com>
2024-11-16 14:28:08 +0700

    Removed onchainkit provider.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import "@coinbase/onchainkit/styles.css";
 import { Providers } from "@/providers/Providers";
 
 const geistSans = localFont({
--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,23 +1,8 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { OnchainKitProvider } from "@coinbase/onchainkit";
-import { WagmiProvider } from "wagmi";
-import { base } from "wagmi/chains";
 import { ReactNode } from "react";
-import { config } from "@/lib/wagmi/config";
 import PrivyProvider from "./PrivyProvider";
 
-const queryClient = new QueryClient();
-
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider chain={base}>
-          <PrivyProvider>{children}</PrivyProvider>
-        </OnchainKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+  return <PrivyProvider>{children}</PrivyProvider>;
 }
