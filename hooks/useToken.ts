015ef484180cd277e6e85f1742f7f9d8d79ecaa7
Sweets Sweetman <sweetmantech@gmail.com>
2024-11-16 16:13:03 +0700

    I remove unused hook to fix build errors.

--- hooks/useToken.ts
+++
@@ -1,7 +0,0 @@
-export function useToken() {
-  const context = useContext(TokenContext);
-  if (context === undefined) {
-    throw new Error("useToken must be used within a TokenProvider");
-  }
-  return context;
-}
