46a69356fd600e23340b4694e4d16748589258f2
Sweets Sweetman <sweetmantech@gmail.com>
2025-01-21 21:15:15 -0300

    P1: TESTNET const    - frontend needs a const called IS_TESTNET    - value of IS_TESTNET defaults to false    - can be overwritten to true with process.env.NEXT_PUBLIC_IS_TESTNET    - deployment network changes based on value of IS_TESTNET      - true - baseSepolia      - false - base

--- lib/consts.ts
+++ lib/consts.ts
@@ -2,10 +2,13 @@ export const COLLECTION_ADDRESS = "0xf0b2ab81056c8e2fdc40e46e32fae895f809c90d";
 export const MINT_FEE_RECIPIENT = "0xcfBf34d385EA2d5Eb947063b67eA226dcDA3DC38";
 export const JSON_EXTENSION_REGISTRY =
   "0xabcdefed93200601e1dfe26d6644758801d732e8";
+import { baseSepolia, base } from "viem/chains";
 
-import { baseSepolia } from "wagmi/chains";
+export const IS_TESTNET =
+  process.env.NEXT_PUBLIC_IS_TESTNET === "true" ? true : false;
+
 // Wagmi
-export const CHAIN = baseSepolia;
+export const CHAIN = IS_TESTNET ? baseSepolia : base;
 export const CHAIN_ID = CHAIN.id;
 // Zora
 export const REFERRAL_RECIPIENT = "0x749B7b7A6944d72266Be9500FC8C221B6A7554Ce";
