c822deed8b31aa443215829929b9cca1e0d7504e
dev <tech.eng.me@gmail.com>
2025-02-11 08:57:45 -0800

    I add loading effect

--- components/ArtistPage/Feed.tsx
+++ components/ArtistPage/Feed.tsx
@@ -1,15 +1,14 @@
 import { useArtistFeed } from "@/hooks/useArtistFeed";
 import HorizontalFeed from "../HorizontalFeed";
 import { FC } from "react";
+import { Skeleton } from "../ui/skeleton";
 
-const Feed: FC<void>  = () => {
+const Feed: FC<void> = () => {
   const { isLoading, data, error } = useArtistFeed();
 
-  if (isLoading) 
-  if (error) {
+  if (isLoading) return <Skeleton className="w-full max-w-4xl mx-auto h-20" />;
+  if (error)
     return <p className="text-center text-red-500 py-4">Failed to load feed</p>;
-  }
-
   return <HorizontalFeed feed={data || []} />;
 };
 
--- components/LandingPage/Feed.tsx
+++ components/LandingPage/Feed.tsx
@@ -3,11 +3,12 @@ import HorizontalFeed from "../HorizontalFeed";
 import { Skeleton } from "../ui/skeleton";
 import { FC } from "react";
 
-const Feed: FC<void>  = () => {
+const Feed: FC<void> = () => {
   const { isLoading, data, error } = useLatestFeed();
 
-  if (isLoading) return <Skeleton  className="w-full max-w-4xl mx-auto h-20" />
-  if (error) return <p className="text-center text-red-500 py-4">Failed to load feed</p>;
+  if (isLoading) return <Skeleton className="w-full max-w-4xl mx-auto h-20" />;
+  if (error)
+    return <p className="text-center text-red-500 py-4">Failed to load feed</p>;
   return <HorizontalFeed feed={data || []} />;
 };
 
