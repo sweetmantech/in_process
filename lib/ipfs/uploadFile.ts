ff1fc4d778eac63ef1b43a126d5b7b7e7171b5fb
dev <tech.eng.me@gmail.com>
2025-02-14 06:57:59 -0800

    fix: route

--- lib/ipfs/uploadFile.ts
+++ lib/ipfs/uploadFile.ts
@@ -2,7 +2,7 @@ export const uploadFile = async (file: File): Promise<string> => {
   try {
     const data = new FormData();
     data.set("file", file);
-    const res = await fetch("/api/ipfs", { method: "POST", body: data });
+    const res = await fetch("/api/arweave", { method: "POST", body: data });
     const arweave_url = await res.json();
 
     return arweave_url;
