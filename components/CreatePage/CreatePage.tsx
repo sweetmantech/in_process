d19ec54e171f3db8727682c614fafac72d43e686
dev <tech.eng.me@gmail.com>
2025-02-18 02:24:36 -0800

    feat: title disappear

--- components/CreatePage/CreatePage.tsx
+++ components/CreatePage/CreatePage.tsx
@@ -5,34 +5,37 @@ import Spinner from "@/components/ui/spinner";
 import MediaUpload from "@/components/MediaUpload/MediaUpload";
 import Title from "./Title";
 import CreateButton from "./CreateButton";
+import BgNoiseWrapper from "../ui/texture-wrapper";
 
 export default function CreatePage() {
   const { creating, name } = useZoraCreateProvider();
 
-  if (creating) {
-    return (
-      <>
-        <Spinner />
-        <span>Creating Post!</span>
-      </>
-    );
-  }
-
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 min-h-screen flex items-center justify-center">
-      <div className="mt-8 md:flex md:space-x-8 w-full">
-        <div className="md:w-1/4 flex flex-col items-center gap-5">
-          <MediaUpload />
-        </div>
-        {name !== null && (
-          <div className="mt-4 md:mt-0 md:w-1/2 flex flex-col items-center gap-3">
-            <div className="w-full flex flex-col items-start gap-4">
-              <Title />
+    <main className="w-screen h-screen flex justify-center items-center bg-gradientTopRightLight">
+      <BgNoiseWrapper url="/egg-shell-noise.png">
+        {creating ? (
+          <div className="flex flex-col gap-2 items-center">
+            <Spinner />
+            <span>Creating Post!</span>
+          </div>
+        ) : (
+          <div className="mx-auto min-h-screen flex items-center justify-center">
+            <div className="mt-8 md:flex md:space-x-8 w-full">
+              <div className="flex flex-col items-center gap-5">
+                <MediaUpload />
+              </div>
+              {typeof name !== "undefined" && (
+                <div className="mt-4 md:mt-0 flex flex-col items-center gap-3">
+                  <div className="w-full flex flex-col items-start gap-4">
+                    <Title />
+                  </div>
+                </div>
+              )}
+              <CreateButton />
             </div>
           </div>
         )}
-        <CreateButton />
-      </div>
-    </div>
+      </BgNoiseWrapper>
+    </main>
   );
 }
