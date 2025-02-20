d04a431959a7de35375b9c68a875738c02a177be
dev <tech.eng.me@gmail.com>
2025-02-20 03:51:28 -0800

    fix: build error

--- providers/CrossmintProvider.tsx
+++ providers/CrossmintProvider.tsx
@@ -4,12 +4,20 @@ import {
   CrossmintProvider as Crossmint,
   CrossmintCheckoutProvider,
 } from "@crossmint/client-sdk-react-ui";
+import { Fragment } from "react";
 
 export default function CrossmintProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  if (!process.env.NEXT_PUBLIC_CROSSMINT_API_KEY)
+    return (
+      <Fragment>
+        NEXT_PUBLIC_CROSSMINT_API_KEY environment variable is not set
+      </Fragment>
+    );
+
   return (
     <Crossmint apiKey={process.env.NEXT_PUBLIC_CROSSMINT_API_KEY as string}>
       <CrossmintCheckoutProvider>{children}</CrossmintCheckoutProvider>
