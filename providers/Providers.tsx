3d8ae0a78e401cf9e80baeed73b98e3a0284b0be
dev <tech.eng.me@gmail.com>
2025-02-20 01:47:43 -0800

    fix: build error

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -4,26 +4,19 @@ import { ReactNode } from "react";
 import PrivyProvider from "./PrivyProvider";
 import { WagmiProvider } from "./WagmiProvider";
 import { ZoraCreateProvider } from "./ZoraCreateProvider";
-import {
-  CrossmintProvider,
-  CrossmintCheckoutProvider,
-} from "@crossmint/client-sdk-react-ui";
 import UserProvider from "./UserProvider";
+import CrossmintProvider from "./CrossmintProvider";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <CrossmintProvider
-      apiKey={process.env.NEXT_PUBLIC_CROSSMINT_API_KEY as string}
-    >
-      <CrossmintCheckoutProvider>
-        <WagmiProvider>
-          <PrivyProvider>
-            <UserProvider>
-              <ZoraCreateProvider>{children}</ZoraCreateProvider>
-            </UserProvider>
-          </PrivyProvider>
-        </WagmiProvider>
-      </CrossmintCheckoutProvider>
+    <CrossmintProvider>
+      <WagmiProvider>
+        <PrivyProvider>
+          <UserProvider>
+            <ZoraCreateProvider>{children}</ZoraCreateProvider>
+          </UserProvider>
+        </PrivyProvider>
+      </WagmiProvider>
     </CrossmintProvider>
   );
 }
