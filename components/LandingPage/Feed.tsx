3df8b2e3a8475cbfa642e6cceffb2b6eb18ba433
dev <tech.eng.me@gmail.com>
2025-02-27 07:07:13 -0800

    feat: 3 demo image

--- components/LandingPage/Feed.tsx
+++ components/LandingPage/Feed.tsx
@@ -1,3 +1,4 @@
+// eslint-disable @next/next/no-img-element jsx-a11y/alt-text
 import { useLatestFeed } from "@/hooks/useLatestFeed";
 import { Skeleton } from "../ui/skeleton";
 import FeedTable from "../FeedTable";
@@ -11,10 +12,17 @@ const Feed = () => {
   if (isLoading) return <Skeleton className="w-full h-20" />;
   return (
     <div className="pt-20">
-      <SpiralFeeds feeds={data || []} />
-      <div className="w-full grid grid-cols-12 pb-6">
-        <div className="col-span-9">
+      <SpiralFeeds feeds={data || []} className="relative z-[2] pr-20" />
+      <div className="w-full grid grid-cols-12 pb-6 gap-6 relative z-[1]">
+        <div className="col-span-9 pr-20">
           <FeedTable feeds={data || []} />
+        </div>
+        <div className="col-span-3 relative">
+          <div className="absolute bottom-0 flex flex-col gap-6">
+            <img src="images/1.png" />
+            <img src="images/1.png" />
+            <img src="images/1.png" />
+          </div>
         </div>
       </div>
     </div>
