a5e9bd597275b38087584ca3a06b9e727f2cc5d9
dev <tech.eng.me@gmail.com>
2025-03-04 10:07:05 -0800

    add semi image

--- components/CreatePage/Buttons.tsx
+++ components/CreatePage/Buttons.tsx
@@ -1,15 +1,24 @@
+import Image from "next/image";
+
 const Buttons = () => {
   return (
-    <div className="space-y-2 pt-3">
+    <div className="space-y-2 pt-3 relative">
+      <div className="absolute w-1/2 aspect-[1/1] z-[2] -right-10 bottom-10 z-[1]">
+        <Image
+          src="/semi-transparent image.png"
+          alt="not found semi"
+          layout="fill"
+        />
+      </div>
       <button
         type="button"
-        className="w-full py-2 bg-black font-archivo text-tan-primary rounded-sm"
+        className="w-full py-2 bg-black font-archivo text-tan-primary rounded-sm z-[2] relative"
       >
         Share
       </button>
       <button
         type="button"
-        className="w-full py-2 font-archivo text-black border border-black rounded-sm"
+        className="w-full py-2 font-archivo text-black border border-black rounded-sm z-[2] relative"
       >
         Create
       </button>
