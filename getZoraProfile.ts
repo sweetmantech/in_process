1e502a6f49625b42fd9735d2e96cbbea5d19f70a
dev <tech.eng.me@gmail.com>
2025-03-04 03:13:33 -0800

    feat: manifesto link

--- getZoraProfile.ts
+++
@@ -1,6 +0,0 @@
-async function getZoraProfile(address: string) {
-  if (!address) return;
-  const res = await fetch(`https://zora.co/api/profiles/${address}?expandedData=true`);
-  if (!res.ok) throw Error(res.statusText);
-  return await res.json();
-} 
