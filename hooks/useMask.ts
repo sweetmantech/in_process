8c1b84e9890110aca348786cb17e3d46e6c0d71a
dev <tech.eng.me@gmail.com>
2025-03-04 08:06:54 -0800

    feat: create mode

---
+++ hooks/useMask.ts
@@ -0,0 +1,50 @@
+import { useZoraCreateProvider } from "@/providers/ZoraCreateProvider";
+import { useEffect, useId, useRef, useState } from "react";
+
+export const useMask = () => {
+  const inputRef = useRef<HTMLDivElement>(null);
+  const titleRef = useRef<HTMLDivElement>(null);
+  const svgRef = useRef<SVGSVGElement>(null);
+  const maskId = useId();
+  const [masks, setMasks] = useState<
+    Array<{ x: number; y: number; width: number; height: number }>
+  >([]);
+  const MASK_PADDING = 20;
+  const { createModeActive } = useZoraCreateProvider();
+
+  useEffect(() => {
+    const updateMasks = () => {
+      if (!svgRef.current) return;
+      const svgRect = svgRef.current.getBoundingClientRect();
+      const newMasks = [];
+
+      if (inputRef.current) {
+        const rect = inputRef.current.getBoundingClientRect();
+        newMasks.push({
+          x: rect.left - svgRect.left - MASK_PADDING,
+          y: rect.top - svgRect.top,
+          width: rect.width + MASK_PADDING * 2,
+          height: rect.height,
+        });
+      }
+
+      if (titleRef.current) {
+        const rect = titleRef.current.getBoundingClientRect();
+        newMasks.push({
+          x: rect.left - svgRect.left - MASK_PADDING,
+          y: rect.top - svgRect.top,
+          width: rect.width + MASK_PADDING * 2,
+          height: rect.height,
+        });
+      }
+
+      setMasks(newMasks);
+    };
+
+    updateMasks();
+    window.addEventListener("resize", updateMasks);
+    return () => window.removeEventListener("resize", updateMasks);
+  }, [inputRef, titleRef, createModeActive]);
+
+  return { masks, svgRef, maskId, inputRef, titleRef };
+};
