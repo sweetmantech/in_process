5b6afadc39514af3e6c88dbc210369df15d71fe3
dev <tech.eng.me@gmail.com>
2025-03-04 10:09:00 -0800

    fix file name

--- components/CreatePage/Buttons.tsx
+++ components/CreatePage/Buttons.tsx
@@ -3,22 +3,24 @@ import Image from "next/image";
 const Buttons = () => {
   return (
     <div className="space-y-2 pt-3 relative">
-      <div className="absolute w-1/2 aspect-[1/1] z-[2] -right-10 bottom-10 z-[1]">
+      <div className="absolute w-1/2 aspect-[1/1] -right-10 bottom-10">
         <Image
-          src="/semi-transparent image.png"
+          src="/semi-transparent.png"
           alt="not found semi"
           layout="fill"
+          objectFit="cover"
+          objectPosition="center"
         />
       </div>
       <button
         type="button"
-        className="w-full py-2 bg-black font-archivo text-tan-primary rounded-sm z-[2] relative"
+        className="w-full py-2 bg-black font-archivo text-tan-primary rounded-sm relative"
       >
         Share
       </button>
       <button
         type="button"
-        className="w-full py-2 font-archivo text-black border border-black rounded-sm z-[2] relative"
+        className="w-full py-2 font-archivo text-black border border-black rounded-sm relative"
       >
         Create
       </button>
