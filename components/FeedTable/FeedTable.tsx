97c3c54094e74460eb383368b565946787d146f1
dev <tech.eng.me@gmail.com>
2025-03-05 16:04:39 -0800

    feat: no scrollbar

--- components/FeedTable/FeedTable.tsx
+++ components/FeedTable/FeedTable.tsx
@@ -1,12 +1,5 @@
 "use client";
-import {
-  ChevronLeft,
-  ChevronRight,
-  ChevronsLeft,
-  ChevronsRight,
-} from "lucide-react";
 
-import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { Collection } from "@/types/token";
 import useFeedTable from "@/hooks/useFeedTable";
@@ -31,7 +24,7 @@ export default function FeedTable({ feeds }: FeedTableProps) {
   const fontSizes = ["text-xl", "text-lg", "text-lg", "text-md"];
   return (
     <div className="w-full">
-      <div className="rounded-md">
+      <div className="rounded-md overflow-auto max-h-[600px] no-scrollbar">
         <Table>
           <TableBody>
             {table.getRowModel().rows.map((row) => (
@@ -69,49 +62,6 @@ export default function FeedTable({ feeds }: FeedTableProps) {
             ))}
           </TableBody>
         </Table>
-      </div>
-      <div className="flex items-center justify-between px-2 mt-4">
-        <div className="flex items-center space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.setPageIndex(0)}
-            disabled={!table.getCanPreviousPage()}
-          >
-            <ChevronsLeft className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            <ChevronLeft className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            <ChevronRight className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-            disabled={!table.getCanNextPage()}
-          >
-            <ChevronsRight className="h-4 w-4" />
-          </Button>
-        </div>
-        <span className="flex items-center gap-1 text-sm font-grotesk-light">
-          <div>Page</div>
-          <strong>
-            {table.getState().pagination.pageIndex + 1} of{" "}
-            {table.getPageCount()}
-          </strong>
-        </span>
       </div>
     </div>
   );
