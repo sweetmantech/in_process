bbf1d3ef2c52ac39bd848a491d24001a3cae06b0
dev <tech.eng.me@gmail.com>
2025-03-06 22:57:12 -0800

    feat: build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import LoginButton from "@/components/LoginButton";
 import Logo from "@/components/Logo";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/sonner";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "In Process",
@@ -20,13 +21,15 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-[url('/bg.png')] bg-cover">
       <body className="min-h-screen flex flex-col !overflow-x-hidden w-screen">
-        <Providers>
-          <Logo className="fixed left-10 top-10 z-10" />
-          <LoginButton className="fixed right-10 top-8 z-10" />
-          <div className="grow relative flex flex-col">{children}</div>
-          <Footer />
-        </Providers>
-        <Toaster />
+        <Suspense>
+          <Providers>
+            <Logo className="fixed left-10 top-10 z-10" />
+            <LoginButton className="fixed right-10 top-8 z-10" />
+            <div className="grow relative flex flex-col">{children}</div>
+            <Footer />
+          </Providers>
+          <Toaster />
+        </Suspense>
       </body>
     </html>
   );
