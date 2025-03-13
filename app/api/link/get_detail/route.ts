28a104ae7157c44b14d4fdd04eeadb57fda94ed9
dev <tech.eng.me@gmail.com>
2025-03-12 18:07:23 -0700

    feat: link input page

---
+++ app/api/link/get_detail/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest } from "next/server";
+import { getLinkPreview } from "link-preview-js";
+import dns from "node:dns";
+
+export async function GET(req: NextRequest) {
+  const url = req.nextUrl.searchParams.get("url");
+  try {
+    if (!url) throw Error("url is invalid.");
+
+    const data = await getLinkPreview(url, {
+      followRedirects: `manual`,
+      handleRedirects: (baseURL: string, forwardedURL: string) => {
+        const urlObj = new URL(baseURL);
+        const forwardedURLObj = new URL(forwardedURL);
+        if (
+          forwardedURLObj.hostname === urlObj.hostname ||
+          forwardedURLObj.hostname === "www." + urlObj.hostname ||
+          "www." + forwardedURLObj.hostname === urlObj.hostname
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      resolveDNSHost: async (url: string) => {
+        return new Promise((resolve, reject) => {
+          const hostname = new URL(url).hostname;
+          dns.lookup(hostname, (err, address) => {
+            if (err) {
+              reject(err);
+              return;
+            }
+
+            resolve(address);
+          });
+        });
+      },
+    });
+    return Response.json(data);
+  } catch (e: any) {
+    console.log(e);
+    const message = e?.message ?? "failed to get Dune transactions";
+    return Response.json({ message }, { status: 500 });
+  }
+}
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
--- components/LinkPage/LinkPage.tsx
+++ components/LinkPage/LinkPage.tsx
@@ -1,3 +1,37 @@
-const LinkPage = () => <div />;
+import useLinkPreview from "@/hooks/useLinkPreview";
+import Image from "next/image";
+import { Skeleton } from "../ui/skeleton";
+
+const LinkPage = () => {
+  const { fetchQuery, link, setLink } = useLinkPreview();
+  const { isLoading, data } = fetchQuery;
+
+  if (isLoading) return <Skeleton className="w-[300px] aspect-[1/1]" />;
+
+  return (
+    <main className="w-screen h-screen flex flex-col gap-6 items-center justify-center">
+      <input
+        type="text"
+        value={link}
+        onChange={(e) => setLink(e.target.value)}
+      />
+      {data && (
+        <>
+          <div className="w-[300px] aspect-[1/1] relative">
+            <Image
+              src={data?.images?.[0] || ""}
+              alt="not found preview image"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+            />
+          </div>
+          <p>{data.title}</p>
+          <p>{data.description}</p>
+        </>
+      )}
+    </main>
+  );
+};
 
 export default LinkPage;
