07f86e0277fb2d31bc9c197b3cbfe17b9aadffac
dev <tech.eng.me@gmail.com>
2025-03-13 21:35:40 -0700

    feat: freeform with dropdown

--- components/CreateForm/Title.tsx
+++ components/CreateForm/Title.tsx
@@ -1,52 +1,28 @@
 import { useZoraCreateProvider } from "@/providers/ZoraCreateProvider";
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { usePathname } from "next/navigation";
-import { Input } from "../ui/input";
+import { ComboboxInput } from "../ui/combobox-input";
 
+const promptOptions = [
+  { label: "this is time when...", value: "this is time when " },
+  { label: "today i...", value: "today i " },
+  { label: "yesterday i...", value: "yesterday i " },
+  { label: "i...", value: "i " },
+  { label: "write anything", value: "write anything " },
+];
 const Title = () => {
   const { name, setName, fileUploading } = useZoraCreateProvider();
-  const pathname = usePathname();
-
-  const isLinkCreation = pathname.includes("/create/link");
 
   return (
     <div className="flex flex-col items-start w-full gap-2">
       <Label htmlFor="title" className="font-archivo text-md">
         prompt
       </Label>
-      {isLinkCreation ? (
-        <Input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="!bg-white !border-grey-secondary !rounded-[0px] !font-spectral !ring-0 !ring-offset-0"
-        />
-      ) : (
-        <Select
-          onValueChange={(value) => setName(value)}
-          value={name}
-          disabled={fileUploading}
-        >
-          <SelectTrigger className="!text-grey-primary !font-spectral w-full bg-white !border !border-grey-secondary !rounded-[0px] !ring-0">
-            <SelectValue className="!rounded-0" />
-          </SelectTrigger>
-          <SelectContent className="!font-spectral text-md text-grey-primary">
-            <SelectItem value="this is time when...">
-              this is time when...
-            </SelectItem>
-            <SelectItem value="today i...">today i...</SelectItem>
-            <SelectItem value="yesterday i...">yesterday i...</SelectItem>
-            <SelectItem value="i...">i...</SelectItem>
-            <SelectItem value="write anything">write anything</SelectItem>
-          </SelectContent>
-        </Select>
-      )}
+      <ComboboxInput
+        onChange={(value) => setName(value)}
+        options={promptOptions}
+        value={name}
+        disabled={fileUploading}
+      />
     </div>
   );
 };
