166210226dd00eebe3088d805b671d378b471d4e
dev <tech.eng.me@gmail.com>
2025-03-17 13:05:57 -0700

    feat: artist page seo image

--- app/[artistAddress]/page.tsx
+++ app/[artistAddress]/page.tsx
@@ -1,5 +1,17 @@
-import { NextPage } from "next";
+import { Metadata, NextPage } from "next";
 import ArtistPage from "@/components/ArtistPage";
+
+export const metadata: Metadata = {
+  title: "In Process",
+  description: "Imagined by LATASHÁ",
+  openGraph: {
+    title: "In Process",
+    description: "Imagined by LATASHÁ",
+    images: [
+      `https://in-process-seven.vercel.app/api/og/artist?artistAddress=0x323e8BCB41ae2454c3f4899e094c599AaB6b84BC`,
+    ],
+  },
+};
 
 const Artist: NextPage = () => <ArtistPage />;
 
