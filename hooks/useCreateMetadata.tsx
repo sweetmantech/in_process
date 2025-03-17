5bdf2b8409d567b21ea79b645cc3037dbe2eb3a7
dev <tech.eng.me@gmail.com>
2025-03-17 07:58:00 -0700

    feat: fix typo

--- hooks/useCreateMetadata.tsx
+++ hooks/useCreateMetadata.tsx
@@ -7,7 +7,7 @@ import useLinkPreview from "./useLinkPreview";
 
 const useCreateMetadata = () => {
   const [link, setLink] = useState<string>("");
-  const [name, setName] = useState<string>("this is time when...");
+  const [name, setName] = useState<string>("this is the time when...");
   const [priceUnit, setPriceUnit] = useState<"eth" | "usd" | "base" | "usdc">(
     "eth",
   );
