12b3a9e5811da5e11ccdcfd75131b28ef8ae9446
dev <tech.eng.me@gmail.com>
2025-03-21 01:23:44 -0700

    feat: undefined issue

--- components/LoginButton/LoginButton.tsx
+++ components/LoginButton/LoginButton.tsx
@@ -8,18 +8,27 @@ interface LoginButtonProps {
   className?: string;
 }
 export function LoginButton({ className = "" }: LoginButtonProps) {
-  const { login, ready, authenticated, logout } = usePrivy();
+  const { login, ready, logout, authenticated, user, linkWallet } = usePrivy();
   const { connectedWallet } = useConnectedWallet();
 
   if (!ready) return null;
 
+  const handleClick = async () => {
+    if (!connectedWallet) {
+      if (authenticated && user?.linkedAccounts?.[0]?.type === "wallet")
+        linkWallet();
+      else login();
+      return;
+    }
+    logout();
+  };
   return (
     <button
       type="button"
-      onClick={authenticated ? logout : login}
+      onClick={handleClick}
       className={`px-3 py-2 bg-red-dark hover:bg-red hover:shadow-[0px_1px_1px_1px_#0000002e] text-white font-archivo lowercase text-sm md:text-base rounded-xs md:rounded-sm ${className}`}
     >
-      {authenticated
+      {connectedWallet
         ? `${truncateAddress(connectedWallet as string)}`
         : "connect wallet"}
     </button>
