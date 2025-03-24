ec6d57e14d29617fcd9378c482fac27abbf75ee4
dev <tech.eng.me@gmail.com>
2025-03-24 05:14:49 -0700

    feat: writing grid

--- components/WritingPage/MaskLines.tsx
+++ components/WritingPage/MaskLines.tsx
@@ -1,10 +1,27 @@
 import { useZoraCreateProvider } from "@/providers/ZoraCreateProvider";
+import React from "react";
 
 const MaskLines = () => {
   const { maskId, svgRef, masks } = useZoraCreateProvider();
 
   return (
     <>
+      <div className="absolute inset-0 pointer-events-none border-b border-b-[#605F5C]" />
+      <div
+        className="absolute inset-0 pointer-events-none"
+        style={{
+          backgroundImage: `repeating-linear-gradient(
+                    to right,
+                    transparent,
+                    transparent 24px,
+                    #605F5C 24px,
+                    #605F5C 25px
+                  )`,
+          backgroundSize: "25px 100%",
+          maskImage: `url(#${maskId})`,
+          WebkitMaskImage: `url(#${maskId})`,
+        }}
+      />
       <div
         className="absolute inset-0 pointer-events-none"
         style={{
@@ -28,14 +45,15 @@ const MaskLines = () => {
           <mask id={maskId}>
             <rect width="100%" height="100%" fill="white" />
             {masks.map((mask, i) => (
-              <rect
-                key={i}
-                x={mask.x}
-                y={mask.y}
-                width={mask.width}
-                height={mask.height}
-                fill="black"
-              />
+              <React.Fragment key={i}>
+                <rect
+                  x={mask.x}
+                  y={mask.y}
+                  width={mask.width}
+                  height={mask.height}
+                  fill="black"
+                />
+              </React.Fragment>
             ))}
           </mask>
         </defs>
