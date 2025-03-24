b026b79f816e3aa08044dfeabb6839abacc54b6a
dev <tech.eng.me@gmail.com>
2025-03-24 03:26:14 -0700

    feat: artist number

--- lib/getArtistsCount.ts
+++ lib/getArtistsCount.ts
@@ -8,7 +8,15 @@ const getArtistsCounts = (feeds: Collection[]) => {
       new Date(feed.released_at).getTime() >=
         todayEpochTime - 24 * 60 * 60 * 1000,
   );
-  return filtred.length;
+  const creatorCounts = filtred.reduce(
+    (acc, feed) => {
+      acc[feed.creator] = (acc[feed.creator] || 0) + 1;
+      return acc;
+    },
+    {} as Record<string, number>,
+  );
+
+  return Object.keys(creatorCounts).length;
 };
 
 export default getArtistsCounts;
