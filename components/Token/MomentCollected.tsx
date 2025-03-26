bffdd0593147a285804aa7cc67738709aeaea832
dev <tech.eng.me@gmail.com>
2025-03-26 07:14:55 -0700

    feat: create

--- components/Token/MomentCollected.tsx
+++ components/Token/MomentCollected.tsx
@@ -2,12 +2,10 @@ import Buttons from "./Buttons";
 
 const MomentCollected = () => {
   return (
-    <div>
-      <div className="flex items-end gap-3 w-full w-fit">
-        <div className="w-full">
-          <p className="font-archivo-medium text-5xl pb-4">moment created</p>
-          <Buttons />
-        </div>
+    <div className="w-full md:max-w-[400px] h-fit">
+      <div className="w-full">
+        <p className="font-archivo-medium text-5xl pb-4">moment created</p>
+        <Buttons />
       </div>
     </div>
   );
