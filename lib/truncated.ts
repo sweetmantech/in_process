858f5db3992d329686e9aeffdbba4d0b76fa3504
dev <tech.eng.me@gmail.com>
2025-03-27 23:33:51 -0700

    feat: slice 20

--- lib/truncated.ts
+++ lib/truncated.ts
@@ -1,5 +1,5 @@
 const truncated = (text: string) => {
-  return text.length > 20 ? `${text}...` : text;
+  return text.length > 20 ? `${text.slice(0, 20)}...` : text;
 };
 
 export default truncated;
