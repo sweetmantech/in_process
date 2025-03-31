64e6745b8a7bb8094354d0ec031aee16e1b54ad1
dev <tech.eng.me@gmail.com>
2025-03-30 22:52:08 -0700

    fix background

--- components/MetadataCreation/TextInput.tsx
+++ components/MetadataCreation/TextInput.tsx
@@ -14,7 +14,7 @@ const TextInput = () => {
   return (
     <div className="overflow-hidden size-full !font-spectral shadow-lg bg-white disabled:cursor-not-allowed relative">
       <textarea
-        className="relative z-[2] size-full !outline-none p-4 opacity-1"
+        className="relative z-[2] size-full !outline-none p-2 md:p-4 bg-white"
         value={writingText}
         disabled={Boolean(fileUploading || creating)}
         onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
@@ -25,7 +25,7 @@ const TextInput = () => {
         }}
       />
       <div
-        className="p-4 absolute z-[1] left-0 top-0 bg-white"
+        className="p-2 md:p-4 absolute z-[1] left-0 top-0 bg-white"
         ref={writingRef}
         dangerouslySetInnerHTML={{
           __html: writingText.replaceAll("\n", "<br/>"),
