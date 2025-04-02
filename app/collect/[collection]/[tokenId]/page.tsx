4a766edf0ecb03c905732147b74fcaf7bf9d4914
dev <tech.eng.me@gmail.com>
2025-04-02 05:35:15 -0700

    update v1 as v2

--- app/collect/[collection]/[tokenId]/page.tsx
+++ app/collect/[collection]/[tokenId]/page.tsx
@@ -1,29 +1,13 @@
 import TokenPage from "@/components/TokenPage";
 import { VERCEL_OG } from "@/lib/consts";
 import { Metadata, NextPage } from "next";
-import { getFrameMetadata } from "@coinbase/onchainkit/frame";
-
-const frameMetadata = (collection: string, tokenId: string) => {
-  // eslint-disable-next-line
-  const [_, address] = collection.split("%3A");
-  return getFrameMetadata({
-    buttons: [
-      {
-        action: "link",
-        label: "Collect",
-        target: `https://inprocess.myco.wtf/collect/${collection}/1`,
-      },
-    ],
-    image: {
-      src: `${VERCEL_OG}/api/og/token?collection=${address}&tokenId=${tokenId}`,
-      aspectRatio: "1.91:1",
-    },
-  });
-};
 
 type Props = {
   params: Promise<{ collection: string; tokenId: string }>;
 };
+
+export const revalidate = 300;
+
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { tokenId, collection } = await params;
   // eslint-disable-next-line
@@ -34,6 +18,22 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
   const title = data.metadata?.name || "In Process";
   const description = data.metadata?.description || "Imagined by LATASHÁ";
+
+  const frame = {
+    version: "next",
+    imageUrl: `${VERCEL_OG}/api/og/token?collection=${address}&tokenId=${tokenId}`,
+    aspectRatio: "1.91:1",
+    button: {
+      title: "Collect",
+      action: {
+        type: "launch_frame",
+        name: title.slice(0, 32),
+        url: `${VERCEL_OG}/collect/${collection}/${tokenId}`,
+        splashImageUrl: `${VERCEL_OG}/desktop_footer_logo.png`,
+        splashBackgroundColor: "#f7f7f7"
+      },
+    },
+  };
 
   return {
     title,
@@ -46,7 +46,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
       ],
     },
     other: {
-      ...frameMetadata(collection, tokenId),
+      "fc:frame": JSON.stringify(frame),
     },
   };
 }
