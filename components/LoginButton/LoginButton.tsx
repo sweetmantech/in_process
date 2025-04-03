2fc4ec06acd1260be6cd7b5b188dc925996a0fcd
dev <tech.eng.me@gmail.com>
2025-04-03 06:27:02 -0700

    feat: signed username

--- components/LoginButton/LoginButton.tsx
+++ components/LoginButton/LoginButton.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { useArtistProfile } from "@/hooks/useArtistProfile";
 import useConnectedWallet from "@/hooks/useConnectedWallet";
 import truncateAddress from "@/lib/truncateAddress";
 import { usePrivy } from "@privy-io/react-auth";
+import { Address } from "viem";
 
 interface LoginButtonProps {
   className?: string;
@@ -10,6 +12,7 @@ interface LoginButtonProps {
 export function LoginButton({ className = "" }: LoginButtonProps) {
   const { login, ready, logout } = usePrivy();
   const { connectedWallet } = useConnectedWallet();
+  const { data } = useArtistProfile(connectedWallet as Address);
 
   if (!ready) return null;
 
@@ -27,7 +30,7 @@ export function LoginButton({ className = "" }: LoginButtonProps) {
       className={`px-3 py-2 bg-red-dark hover:bg-red hover:shadow-[0px_1px_1px_1px_#0000002e] text-white font-archivo lowercase text-sm md:text-base rounded-xs md:rounded-sm ${className}`}
     >
       {connectedWallet
-        ? `${truncateAddress(connectedWallet as string)}`
+        ? `${data?.username || truncateAddress(connectedWallet as string)}`
         : "connect wallet"}
     </button>
   );
