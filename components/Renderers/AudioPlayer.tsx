e8b248468ee5ffaa312a33179b0f71a21e7acfed
dev <tech.eng.me@gmail.com>
2025-04-06 22:28:06 -0700

    feat: audio player

---
+++ components/Renderers/AudioPlayer.tsx
@@ -0,0 +1,71 @@
+import { useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Pause, Play } from "lucide-react";
+import { Slider } from "@/components/ui/slider";
+
+interface AudioPlayerProps {
+  url: string;
+}
+const AudioPlayer = ({ url }: AudioPlayerProps) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const togglePlayPause = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const handleTimeUpdate = () => {
+    if (audioRef.current) {
+      const progress =
+        (audioRef.current.currentTime / audioRef.current.duration) * 100;
+      setProgress(progress);
+    }
+  };
+
+  const handleSliderChange = (value: number[]) => {
+    if (audioRef.current) {
+      const time = (value[0] / 100) * audioRef.current.duration;
+      audioRef.current.currentTime = time;
+      setProgress(value[0]);
+    }
+  };
+
+  return (
+    <div className="py-6 flex items-center justify-center bg-white rounded-lg shadow-lg overflow-hidden">
+      <div className="px-3 w-full">
+        <audio ref={audioRef} src={url} onTimeUpdate={handleTimeUpdate} />
+        <div className="text-center">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={togglePlayPause}
+            className="text-primary hover:text-primary-dark"
+          >
+            {isPlaying ? (
+              <Pause className="size-6" />
+            ) : (
+              <Play className="size-6" />
+            )}
+          </Button>
+        </div>
+        <Slider
+          value={[progress]}
+          onValueChange={handleSliderChange}
+          max={100}
+          step={1}
+          className="w-full bg-black"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AudioPlayer;
