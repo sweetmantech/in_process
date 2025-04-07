4f84730f063ed4a16fb3a73699b45aee1d2cf8f0
dev <tech.eng.me@gmail.com>
2025-04-07 09:03:44 -0700

    add client arweave upload

--- lib/arweave/clientUploadToArweave.ts
+++ lib/arweave/clientUploadToArweave.ts
@@ -1,23 +1,32 @@
+import {
+  TurboFactory,
+} from '@ardrive/turbo-sdk/web';
+import { ReadableStream } from 'web-streams-polyfill';
+
+TurboFactory.setLogLevel('debug');
 
 const clientUploadToArweave = async (file: File): Promise<string | null> => {
   try {
-    // const ARWEAVE_KEY = JSON.parse(
-    //   Buffer.from(
-    //     process.env.NEXT_PUBLIC_ARWEAVE_KEY as string,
-    //     "base64",
-    //   ).toString(),
-    // );
-    // const signer = new ArweaveSigner(ARWEAVE_KEY)
-
-    // // Create a DataItem from a string
-    // const myStringData: string = 'Hello, Permaweb!'
-    // const myDataItem = createData(myStringData, signer)
+    const ARWEAVE_KEY = JSON.parse(
+      Buffer.from(process.env.NEXT_PUBLIC_ARWEAVE_KEY as string, "base64").toString(),
+    );
     
-    // // Create a DataItem from a Buffer or Uint8Array
-    // const myBufferData: Buffer | Uint8Array = Buffer.from('Hello, Permaweb!')
-    // const myOtherDataItem = createData(myBufferData, signer)
+    const turboClient: any = TurboFactory.authenticated({
+      privateKey: ARWEAVE_KEY,
+    });
+    const buffer = await file.arrayBuffer();
+    const upload = await turboClient.uploadFile({
+      fileStreamFactory: () =>
+        new ReadableStream({
+          start(controller) {
+            controller.enqueue(buffer);
+            controller.close();
+          },
+        }),
+      fileSizeFactory: () => file.size,
+    });
 
-
+    console.log('ziad', `Upload successful! ${JSON.stringify(upload, null, 2)}`)
     return "";
   } catch (error) {
     console.error("Error uploading to Arweave:", error);
