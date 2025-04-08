73f9ab0500b0b9385c4a3c60bd5acd08051eac90
dev <tech.eng.me@gmail.com>
2025-04-08 16:09:39 -0700

    fix arweave port

--- components/OnboardingModal/NavigationDots.tsx
+++ components/OnboardingModal/NavigationDots.tsx
@@ -1,12 +1,15 @@
-import Image from 'next/image';
-import { slides } from './OnboardingModalContent';
+import Image from "next/image";
+import { slides } from "./OnboardingModalContent";
 
 interface NavigationDotsProps {
   currentSlide: number;
   onSlideChange: (index: number) => void;
 }
 
-export const NavigationDots = ({ currentSlide, onSlideChange }: NavigationDotsProps) => (
+export const NavigationDots = ({
+  currentSlide,
+  onSlideChange,
+}: NavigationDotsProps) => (
   <>
     {slides.slice(0, 3).map((_, index) => (
       <button
@@ -17,7 +20,11 @@ export const NavigationDots = ({ currentSlide, onSlideChange }: NavigationDotsPr
         }}
       >
         <Image
-          src={index === currentSlide ? "/images/modal-dot-active.svg" : "images/modal-dot.svg"}
+          src={
+            index === currentSlide
+              ? "/images/modal-dot-active.svg"
+              : "images/modal-dot.svg"
+          }
           alt={`Slide ${index + 1}`}
           width={16}
           height={16}
@@ -27,4 +34,4 @@ export const NavigationDots = ({ currentSlide, onSlideChange }: NavigationDotsPr
       </button>
     ))}
   </>
-); 
+);
