05370ec8d1ac8224537fe39412c9dd9840a0ba5e
dev <tech.eng.me@gmail.com>
2025-04-09 09:07:23 -0700

    feat: created moment

--- hooks/useZoraCreate.tsx
+++ hooks/useZoraCreate.tsx
@@ -6,7 +6,10 @@ import { CHAIN_ID } from "@/lib/consts";
 import { useParams } from "next/navigation";
 import { Address } from "viem";
 import useZoraCreateParameters from "./useZoraCreateParameters";
-import { getContractAddressFromReceipt } from "@/lib/protocolSdk/create/1155-create-helper";
+import {
+  getContractAddressFromReceipt,
+  getTokenIdFromCreateReceipt,
+} from "@/lib/protocolSdk/create/1155-create-helper";
 import { getPublicClient } from "@/lib/viem/publicClient";
 import { useMask } from "./useMask";
 import useBalance from "./useBalance";
@@ -35,8 +38,9 @@ export default function useZoraCreate() {
   const [creating, setCreating] = useState<boolean>(false);
   const params = useParams();
   const chainId = Number(params.chainId) || CHAIN_ID;
-  const collection = params.collection as Address | undefined;
-  const [createdContract, setCreatedContract] = useState("");
+  const collection = params.collectionAddress as Address | undefined;
+  const [createdContract, setCreatedContract] = useState<string>("");
+  const [createdTokenId, setCreatedTokenId] = useState<string>("");
   const { fetchParameters, createMetadata } = useZoraCreateParameters(
     chainId,
     collection,
@@ -61,12 +65,19 @@ export default function useZoraCreate() {
 
       if (!hash) throw new Error();
 
-      const publicClient = getPublicClient();
+      const publicClient: any = getPublicClient();
       const receipt = await publicClient.waitForTransactionReceipt({ hash });
-      const contractAddress = getContractAddressFromReceipt(receipt);
+      if (!collection) {
+        const contractAddress = getContractAddressFromReceipt(receipt);
+        setCreatedContract(contractAddress);
+        setCreatedTokenId("1");
+        return { contractAddress, tokenId: 1 };
+      }
+      const tokenId = getTokenIdFromCreateReceipt(receipt);
       setCreating(false);
-      setCreatedContract(contractAddress);
-      return { contractAddress };
+      setCreatedContract(collection as Address);
+      setCreatedTokenId(tokenId.toString());
+      return { contractAddress: collection, tokenId };
     } catch (err) {
       setCreating(false);
       console.error(err);
@@ -76,6 +87,8 @@ export default function useZoraCreate() {
 
   return {
     createdContract,
+    createdTokenId,
+    setCreatedTokenId,
     setCreatedContract,
     create,
     creating,
