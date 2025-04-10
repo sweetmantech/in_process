bfd5290a16c1959d76e118ec91a35004a83a90c2
dev <tech.eng.me@gmail.com>
2025-04-09 20:59:15 -0700

    commit

--- app/api/dune/tokens/route.ts
+++ app/api/dune/tokens/route.ts
@@ -1,11 +1,27 @@
+import getNextTokenIds, { CollectionAndNextTokenId } from "@/lib/viem/getNextTokenIds";
 import { Collection } from "@/types/token";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
     const collections: Collection[] = body.collections
+
   try {
-    
+    const collectionsAndNextTokenIds = await getNextTokenIds(collections)
+    collectionsAndNextTokenIds.map(async (c: CollectionAndNextTokenId) => {
+        const tokens = [{
+            tokenId: 1,
+            collection: c.newContract,
+            creator: c.creator,
+
+        }]
+        if (c.nextTokenId < 2) {
+            return [{
+                tokenId: 1,
+                nextTokenId:
+            }]
+        }
+    })
     return Response.json([]);
   } catch (e: any) {
     console.log(e);
