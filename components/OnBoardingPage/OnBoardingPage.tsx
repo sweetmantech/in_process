c8812fce31df10e5bd74bb739dff06fe8684f7b5
dev <tech.eng.me@gmail.com>
2025-04-11 06:24:54 -0700

    feat onboarding

---
+++ components/OnBoardingPage/OnBoardingPage.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useFeedProvider } from "@/providers/FeedProvider";
+import Image from "next/image";
+import FeedItem from "./FeedItem";
+import useConnectedWallet from "@/hooks/useConnectedWallet";
+import { redirect } from "next/navigation";
+import { useAccount } from "wagmi";
+import { useFrameProvider } from "@/providers/FrameProvider";
+
+const OnBoardingPage = () => {
+  const { feeds } = useFeedProvider();
+  const { connectedWallet } = useConnectedWallet();
+  const { address } = useAccount();
+  const { context } = useFrameProvider();
+  const signedWallet = context ? address : connectedWallet;
+  if (signedWallet) redirect("/");
+  return (
+    <div className="w-screen flex flex-col items-center pt-[200px]">
+      <p className="font-archivo text-5xl">A Collective Onchain Timeline</p>
+      <p className="font-spectral-italic text-4xl pt-4">for artists</p>
+      <Image
+        alt="not found ico"
+        width={108}
+        height={108}
+        unoptimized
+        src={"/diamond.svg"}
+        className="mt-16"
+      />
+      <div className="w-full grid grid-cols-3">
+        {feeds.slice(0, 3).map((feed, i) => (
+          <FeedItem key={i} feed={feed} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OnBoardingPage;
