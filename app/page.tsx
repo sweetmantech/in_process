d360729a7ef588e3129c9060062cd57c3a7e5711
dev <tech.eng.me@gmail.com>
2025-04-17 09:27:53 -0700

    feat: home page mini app farpcast

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,36 @@
-"use client";
-
 import LandingPage from "@/components/LandingPage/LandingPage";
-import useCheckSign from "@/hooks/useCheckSign";
+import { APP_URL } from "@/lib/consts";
+import { Metadata } from "next";
 
-const HomePage = () => {
-  useCheckSign();
+export const revalidate = 300;
 
-  return <LandingPage />;
+export const metadata: Metadata = {
+  title: "In Process",
+  description: "Imagined by LATASHÁ",
+  openGraph: {
+    title: "In Process",
+    description: "Imagined by LATASHÁ",
+    images: [`${APP_URL}/desktop_footer_logo.png`],
+  },
+  other: {
+    "fc:frame": JSON.stringify({
+      version: "next",
+      imageUrl: `${APP_URL}/desktop_footer_logo.png`,
+      aspectRatio: "1:1",
+      button: {
+        title: "Create",
+        action: {
+          type: "launch_frame",
+          name: "In Process",
+          url: APP_URL,
+          splashImageUrl: `${APP_URL}/desktop_footer_logo.png`,
+          splashBackgroundColor: "#6330d0",
+        },
+      },
+    }),
+  },
 };
+
+const HomePage = () => <LandingPage />;
 
 export default HomePage;
