99347e6f6e80bfda7161409154cfc446c4bf22f2
dev <tech.eng.me@gmail.com>
2025-04-17 08:57:52 -0700

    fix mobile padding x

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Logo from "../Logo";
 
 const Header = () => {
   return (
-    <div className="w-screen flex justify-between items-center px-4 md:px-10 pt-6">
+    <div className="w-screen flex justify-between items-center px-6 md:px-10 pt-6">
       <Logo />
       <LoginButton />
     </div>
