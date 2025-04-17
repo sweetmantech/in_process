3f984fa7400d944df57c39e2bb6d0f20fb913e7c
dev <tech.eng.me@gmail.com>
2025-04-17 08:51:43 -0700

    feat: header padding x

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,10 @@ import type { Metadata } from "next";
 import "@xyflow/react/dist/style.css";
 import "./globals.css";
 import { Providers } from "@/providers/Providers";
-import LoginButton from "@/components/LoginButton";
-import Logo from "@/components/Logo";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/sonner";
 import { Suspense } from "react";
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "In Process",
@@ -23,8 +22,7 @@ export default function RootLayout({
       <body className="min-h-screen flex flex-col !overflow-x-hidden w-screen !lowercase">
         <Suspense>
           <Providers>
-            <Logo className="fixed left-2 md:left-10 top-4 md:top-10 z-10" />
-            <LoginButton className="fixed right-2 md:right-10 top-2 md:top-8 z-10" />
+            <Header />
             <div className="grow relative flex flex-col">{children}</div>
             <Footer />
           </Providers>
