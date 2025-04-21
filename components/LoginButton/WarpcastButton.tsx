a69917bf7813237408c2aa07b18ff87813d8af99
Gonzalo Perez Rojas <0xg@Gonzalos-MBP-2.fibertel.com.ar>
2025-04-21 16:17:04 -0300

    add Dropdown to WarpcastButton

--- components/LoginButton/WarpcastButton.tsx
+++ components/LoginButton/WarpcastButton.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { useState } from "react";
 import truncateAddress from "@/lib/truncateAddress";
 import { useFrameProvider } from "@/providers/FrameProvider";
 import { config } from "@/providers/WagmiProvider";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { DropdownMenu } from "./DropdownMenu";
 
 interface WarpcastButtonProps {
   className?: string;
 }
 export function WarpcastButton({ className = "" }: WarpcastButtonProps) {
+  const [showDropdown, setShowDropdown] = useState(false);
   const { context } = useFrameProvider();
   const { isConnected, address } = useAccount();
   const { connect } = useConnect();
@@ -16,21 +19,29 @@ export function WarpcastButton({ className = "" }: WarpcastButtonProps) {
 
   const handleClick = async () => {
     if (isConnected) {
-      disconnect();
+      setShowDropdown(!showDropdown);
       return;
     }
     connect({ connector: config.connectors[0] });
   };
 
   return (
-    <button
-      type="button"
-      onClick={handleClick}
-      className={`px-3 py-2 bg-red-dark hover:bg-red hover:shadow-[0px_1px_1px_1px_#0000002e] text-white font-archivo lowercase text-sm md:text-base rounded-xs md:rounded-sm ${className}`}
-    >
-      {isConnected
-        ? `${context?.user.displayName || truncateAddress(address as string)}`
-        : "sign in"}
-    </button>
+    <div className="relative">
+      <button
+        type="button"
+        onClick={handleClick}
+        className={`px-3 py-2 bg-red-dark hover:bg-red hover:shadow-[0px_1px_1px_1px_#0000002e] text-white font-archivo lowercase text-sm md:text-base rounded-xs md:rounded-sm ${className}`}
+      >
+        {isConnected
+          ? `${context?.user.displayName || truncateAddress(address as string)}`
+          : "sign in"}
+      </button>
+      {showDropdown && isConnected && (
+        <DropdownMenu onLogout={() => {
+          disconnect();
+          setShowDropdown(false);
+        }} />
+      )}
+    </div>
   );
 }
