75d6ab96b148d3551fe6290b69069d1bf291ed49
Gonzalo Perez Rojas <0xg@Gonzalos-MacBook-Pro-2.local>
2025-04-24 14:54:32 -0300

    new onboarding page design

--- components/OnBoardingPage/FeedItem.tsx
+++ components/OnBoardingPage/FeedItem.tsx
@@ -11,19 +11,17 @@ const FeedItem = ({ feed }: FeedItemProps) => {
   const { data, isLoading } = useMetadata(feed.uri);
   if (isLoading)
     return (
-      <div className="w-full aspect-[1/1] relative flex items-center justify-center mt-14">
+      <div className="w-full aspect-video bg-grey-moss-100 rounded-lg flex items-center justify-center mt-14">
         <Loading className="size-full" />
       </div>
     );
   return (
-    <div className="w-full aspect-[1/1] relative mt-14">
+    <div className="w-full aspect-video relative mt-14 bg-grey-moss-100">
       <Image
         src={getFetchableUrl(data?.image) || ""}
         alt="not found img"
+        fill
         objectFit="contain"
-        objectPosition="center"
-        layout="fill"
-        className="absolute"
       />
     </div>
   );
--- components/OnBoardingPage/OnBoardingPage.tsx
+++ components/OnBoardingPage/OnBoardingPage.tsx
@@ -8,6 +8,7 @@ import { redirect } from "next/navigation";
 import { useAccount } from "wagmi";
 import { useFrameProvider } from "@/providers/FrameProvider";
 import { useEffect } from "react";
+import { Button } from "../ui/button";
 
 const OnBoardingPage = () => {
   const { feeds } = useInProcessFeedProvider();
@@ -25,9 +26,9 @@ const OnBoardingPage = () => {
   }, [signedWalletAddress]);
 
   return (
-    <div className="w-screen flex flex-col items-center pt-[200px]">
+    <div className="w-screen flex flex-col items-center pt-[160px]">
       <p className="font-archivo text-5xl">A Collective Onchain Timeline</p>
-      <p className="font-spectral-italic text-4xl pt-4">for artists</p>
+      <p className="font-spectral-italic text-4xl pt-8">for artists</p>
       <Image
         alt="not found ico"
         width={108}
@@ -36,7 +37,8 @@ const OnBoardingPage = () => {
         src={"/diamond.svg"}
         className="mt-16"
       />
-      <div className="w-full grid grid-cols-3">
+      <Button className="mt-16 px-14 py-2 h-12 text-md bg-grey-moss-900 hover:bg-grey-moss-300 text-[#FFF9EA] text-[22px] font-normal rounded-sm">create</Button>
+      <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4 px-8">
         {feeds.slice(0, 3).map((feed, i) => (
           <FeedItem key={i} feed={feed} />
         ))}
