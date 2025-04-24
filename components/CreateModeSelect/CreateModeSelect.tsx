46c8f3fb5d144b9c70667196039868ae9dded31d
dev <tech.eng.me@gmail.com>
2025-04-24 06:52:31 -0700

    fix query

--- components/CreateModeSelect/CreateModeSelect.tsx
+++ components/CreateModeSelect/CreateModeSelect.tsx
@@ -2,18 +2,20 @@
 
 import { useZoraCreateProvider } from "@/providers/ZoraCreateProvider";
 import CTAButton from "./CTAButton";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import CreatedMoment from "../CreatedMoment/CreatedMoment";
 
 const CreateModeSelect = () => {
   const { titleRef, createdContract } = useZoraCreateProvider();
   const pathname = usePathname();
   const { push } = useRouter();
-
+  const searchParams = useSearchParams();
+  const urlParams = searchParams.toString();
   const isCreatePage = pathname === "/create";
   const isWritingPage = pathname === "/create/writing";
   const isLinkPage = pathname === "/create/link";
   const isEmbedPage = pathname === "/create/embed";
+  const urlQuery = urlParams ? `?${urlParams}` : "";
 
   return (
     <div className="col-span-1">
@@ -22,24 +24,27 @@ const CreateModeSelect = () => {
       ) : (
         <div className="w-full lg:max-w-[250px] xl:max-w-[300px] h-fit">
           <div ref={titleRef} className="flex flex-col gap-3 pb-3">
-            <CTAButton isActive={isCreatePage} onClick={() => push("/create")}>
+            <CTAButton
+              isActive={isCreatePage}
+              onClick={() => push(`/create${urlQuery}`)}
+            >
               new moment
             </CTAButton>
             <CTAButton
               isActive={isWritingPage}
-              onClick={() => push("/create/writing")}
+              onClick={() => push(`/create/writing${urlQuery}`)}
             >
               new thought
             </CTAButton>
             <CTAButton
               isActive={isLinkPage}
-              onClick={() => push("/create/link")}
+              onClick={() => push(`/create/link${urlQuery}`)}
             >
               new link
             </CTAButton>
             <CTAButton
               isActive={isEmbedPage}
-              onClick={() => push("/create/embed")}
+              onClick={() => push(`/create/embed${urlQuery}`)}
             >
               new embed
             </CTAButton>
