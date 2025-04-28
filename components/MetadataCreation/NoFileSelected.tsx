a0c889f41ea2a9a63f575adf3fdc59ea7b18d56b
dev <tech.eng.me@gmail.com>
2025-04-28 08:22:55 -0700

    feat: create moment page mobile ui

--- components/MetadataCreation/NoFileSelected.tsx
+++ components/MetadataCreation/NoFileSelected.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
+
 const NoFileSelected = () => (
   <div className="size-full px-4 md:px-10 pt-10 pointer-events-none">
     <div className="flex flex-col justify-center items-center gap-2 w-full aspect-[1/1] rounded-full border border-grey-moss-400 relative overflow-hidden">
       <div className="size-full absolute left-0 right-0 bg-grey-moss-100 opacity-[0.7]" />
-      <p className="font-archivo-medium text-sm md:text-lg z-[2]">
-        drop an image, video, pdf, link, or embed
+      <p className="font-archivo-medium text-sm md:text-lg z-[2] text-center">
+        drop an image, video, pdf, <br className="block md:hidden" /> link, or
+        embed
       </p>
       <button
         type="button"
@@ -11,6 +14,17 @@ const NoFileSelected = () => (
       >
         choose media
       </button>
+    </div>
+    <div className="flex justify-center py-2">
+      <Image
+        src="/moment.svg"
+        blurDataURL="moment.png"
+        alt="not found moment"
+        unoptimized
+        width={108}
+        height={186}
+        className="self-center"
+      />
     </div>
   </div>
 );
