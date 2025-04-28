254b67e851969213c1bf0ff5f6ce2e080a21ca9a
dev <tech.eng.me@gmail.com>
2025-04-28 05:58:27 -0700

    fix dropdown timeline

--- components/LoginButton/DropdownMenu.tsx
+++ components/LoginButton/DropdownMenu.tsx
@@ -1,5 +1,7 @@
+import useSignedAddress from "@/hooks/useSignedAddress";
 import { useFrameProvider } from "@/providers/FrameProvider";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface DropdownMenuProps {
   onLogout: () => void;
@@ -7,6 +9,8 @@ interface DropdownMenuProps {
 
 export function DropdownMenu({ onLogout }: DropdownMenuProps) {
   const { context } = useFrameProvider();
+  const { push } = useRouter();
+  const signedAddress = useSignedAddress();
 
   return (
     <div className="absolute left-0 right-0 bg-[#1C1C1C] shadow-lg font-archivo z-50 rounded-b-sm border-t-0">
@@ -28,9 +32,12 @@ export function DropdownMenu({ onLogout }: DropdownMenuProps) {
       <div className="flex justify-center">
         <div className="w-4/5 h-px bg-grey-moss-50" />
       </div>
-      <div className="px-4 py-2">
-        <div className="text-white text-base">timeline</div>
-      </div>
+      <button
+        onClick={() => push(`/${signedAddress}`)}
+        className="w-full text-left px-4 py-2 text-white text-base hover:bg-[#333333] hover:rounded-b-sm transition-colors"
+      >
+        timeline
+      </button>
       {!context && (
         <>
           <div className="flex justify-center">
