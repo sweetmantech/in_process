5d10d3aa070e83bf630616daa1d22b5a898de495
dev <tech.eng.me@gmail.com>
2025-04-28 11:19:28 -0700

    fix status

--- components/Moment/Moment.tsx
+++ components/Moment/Moment.tsx
@@ -13,9 +13,12 @@ const Moment = ({ children }: { children: ReactNode }) => {
     if (createdContract) return false;
     return true;
   }, [pathname, isMobile, createdContract]);
+  const isWritingPage = pathname === "/create/writing";
+  const isCreatingPage = pathname === "/create";
+
   return (
     <div
-      className={`w-full ${createdContract ? "min-h-auto" : "min-h-[400px]"} md:min-h-auto md:aspect-[571/692] relative ${gridVisible && "bg-[url('/grid.svg')]"} bg-contain`}
+      className={`w-full ${createdContract ? (isWritingPage || isCreatingPage ? "min-h-[300px]" : "min-h-auto") : "min-h-[400px]"} md:min-h-auto md:aspect-[571/692] relative ${gridVisible && "bg-[url('/grid.svg')]"} bg-contain`}
     >
       {children}
     </div>
