28952ecb823ecc8c7fd4f9593153a7a816c8b2ad
dev <tech.eng.me@gmail.com>
2025-04-29 11:29:12 -0700

    fix usd price

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -10,25 +10,28 @@ import FrameProvider from "./FrameProvider";
 import InProcessFeedProvider from "./InProcessFeedProvider";
 import LayoutProvider from "./LayoutProvider";
 import UserCollectionsProvider from "./UserCollectionsProvider";
+import EthPriceProvider from "./EthPriceProvider";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <WagmiProvider>
-      <CrossmintProvider>
-        <PrivyProvider>
-          <FrameProvider>
-            <UserProvider>
-              <ZoraCreateProvider>
-                <InProcessFeedProvider>
-                  <UserCollectionsProvider>
-                    <LayoutProvider>{children}</LayoutProvider>
-                  </UserCollectionsProvider>
-                </InProcessFeedProvider>
-              </ZoraCreateProvider>
-            </UserProvider>
-          </FrameProvider>
-        </PrivyProvider>
-      </CrossmintProvider>
-    </WagmiProvider>
+    <EthPriceProvider>
+      <WagmiProvider>
+        <CrossmintProvider>
+          <PrivyProvider>
+            <FrameProvider>
+              <UserProvider>
+                <ZoraCreateProvider>
+                  <InProcessFeedProvider>
+                    <UserCollectionsProvider>
+                      <LayoutProvider>{children}</LayoutProvider>
+                    </UserCollectionsProvider>
+                  </InProcessFeedProvider>
+                </ZoraCreateProvider>
+              </UserProvider>
+            </FrameProvider>
+          </PrivyProvider>
+        </CrossmintProvider>
+      </WagmiProvider>
+    </EthPriceProvider>
   );
 }
