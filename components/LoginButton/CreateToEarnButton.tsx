f9f2fd85c3754baae2628305d07bbc1eac160782
dev <tech.eng.me@gmail.com>
2025-05-01 11:49:28 -0700

    hide create to earn button when balance > 0

--- components/LoginButton/CreateToEarnButton.tsx
+++ components/LoginButton/CreateToEarnButton.tsx
@@ -1,10 +1,19 @@
+import { useEthPriceProvider } from "@/providers/EthPriceProvider";
 import { useLayoutProvider } from "@/providers/LayoutProvider";
+import { useUserCollectionsProvider } from "@/providers/UserCollectionsProvider";
 import { useRouter } from "next/navigation";
+import { Fragment } from "react";
 
 const CreateToEarnButton = ({ className = "" }: { className?: string }) => {
   const { push } = useRouter();
   const { toggleNavbar } = useLayoutProvider();
+  const { data } = useUserCollectionsProvider();
+  const { ethPrice } = useEthPriceProvider();
+  const earnings = Number(Number(data?.totalEarnings || 0) * ethPrice).toFixed(
+    2,
+  );
 
+  if (parseFloat(earnings) > 0 || !data?.totalEarnings) return <Fragment />;
   return (
     <button
       type="button"
