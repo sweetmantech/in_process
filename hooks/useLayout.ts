b33efb2aa3e6378d6dccc1b8442734da5b69e2e1
dev <tech.eng.me@gmail.com>
2025-05-01 10:07:21 -0700

    navbar dropdown onclick outside

--- hooks/useLayout.ts
+++ hooks/useLayout.ts
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useSignedAddress from "./useSignedAddress";
 
 const useLayout = () => {
+  const menuRef = useRef() as any;
   const signedAddress = useSignedAddress();
   const [isOpenNavbar, setIsOpenNavbar] = useState<boolean>(false);
   const toggleNavbar = () => {
@@ -9,9 +10,21 @@ const useLayout = () => {
     setIsOpenNavbar(!isOpenNavbar);
   };
 
+  useEffect(() => {
+    const handleClose = (e: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(e.target))
+        setIsOpenNavbar(false);
+    };
+
+    document.addEventListener("mousedown", handleClose);
+
+    return () => document.removeEventListener("mousedown", handleClose);
+  }, [menuRef]);
+
   return {
     toggleNavbar,
     isOpenNavbar,
+    menuRef,
   };
 };
 
