7a41fc04d617b4d8fa01fcc62b9017597326464e
dev <tech.eng.me@gmail.com>
2025-05-01 07:32:08 -0700

    comments section bg color

--- components/Token/CommentsContainer.tsx
+++ components/Token/CommentsContainer.tsx
@@ -4,7 +4,7 @@ const CommentsContainer = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-full mt-2 md:mt-10">
       <p className="text-lg font-archivo">comments</p>
-      <div className="min-h-[300px] rounded-md mt-1 md:mt-4 py-5 px-6 relative rounded-md overflow-hidden bg-[#99999933] bg-cover">
+      <div className="min-h-[300px] rounded-md mt-1 md:mt-4 py-5 px-6 relative rounded-md overflow-hidden bg-[#99999933]">
         {children}
       </div>
     </div>
