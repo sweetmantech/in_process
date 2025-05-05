4838380a3f4eb6eff6893b9779e0d094c995be19
Gonzalo Perez Rojas <0xg@Gonzalos-MacBook-Pro-2.local>
2025-05-05 10:20:28 -0300

    update SEO tags

--- app/manifesto/page.tsx
+++ app/manifesto/page.tsx
@@ -2,15 +2,17 @@ import ManifestoPage from "@/components/ManifestoPage";
 import { APP_URL } from "@/lib/og/consts";
 import { Metadata } from "next";
 
+const title = "Manifesto"
+	
 export const metadata: Metadata = {
-  title: "In Process",
+  title: `${title} - In Process`,
   description: "Imagined by LATASHÁ",
   openGraph: {
-    title: "In Process",
+    title: `${title} - In Process`,
     description: "Imagined by LATASHÁ",
     images: [`${APP_URL}/manifesto_preview.png`],
-  }
-}
+  },
+};
 
 const Manifesto = () => <ManifestoPage />;
 
