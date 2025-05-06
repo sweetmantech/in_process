dce9f79da31a51cec4e39ebec6159fe2827b82a5
dev <tech.eng.me@gmail.com>
2025-05-06 15:36:59 -0700

    Update route.tsx

--- app/api/og/token/route.tsx
+++ app/api/og/token/route.tsx
@@ -1,10 +1,10 @@
 import OgBackground from "@/components/Og/token/OgBackground";
-import OgFooter from "@/components/Og/token/OgFooter";
-import OgHeader from "@/components/Og/token/OgHeader";
+// import OgFooter from "@/components/Og/token/OgFooter";
+// import OgHeader from "@/components/Og/token/OgHeader";
 import { NextRequest, NextResponse } from "next/server";
 import { getFetchableUrl } from "@/lib/protocolSdk/ipfs/gateway";
-import getUsername from "@/lib/getUsername";
-import getUserAvatar from "@/lib/getUserAvatar";
+// import getUsername from "@/lib/getUsername";
+// import getUserAvatar from "@/lib/getUserAvatar";
 import { VERCEL_OG } from "@/lib/og/consts";
 import satori from "satori";
 
@@ -30,57 +30,64 @@ export async function GET(req: NextRequest) {
   if (!tokenId || !collection)
     throw Error("collection or tokenId should be provided.");
 
-  const comments = await fetch(
-    `${VERCEL_OG}/api/dune/mint_comments?tokenContract=${collection}&tokenId=${tokenId}`,
-  ).then((res) => res.json());
+  // const comments = await fetch(
+  //   `${VERCEL_OG}/api/dune/mint_comments?tokenContract=${collection}&tokenId=${tokenId}`,
+  // ).then((res) => res.json());
 
   const metadata = await fetch(
     `${VERCEL_OG}/api/token/metadata?collection=${collection}&tokenId=${tokenId}`,
   ).then((res) => res.json());
 
-  const username = await getUsername(metadata.owner);
-  const useravatar = await getUserAvatar(metadata.owner);
+  // const username = await getUsername(metadata.owner);
+  // const useravatar = await getUserAvatar(metadata.owner);
   const [archivoFontData, spectralFontData] = await Promise.all([
     archivoFont,
     spectralFont,
   ]);
   const svg = await satori(
-    <div
-      style={{
-        display: "flex",
-        position: "relative",
-        overflow: "hidden",
-        width: "100%",
-        height: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <OgBackground
-        backgroundUrl={
-          getFetchableUrl(
-            metadata.metadata.image || `${VERCEL_OG}/images/placeholder.png`,
-          ) || ""
-        }
-      />
-      <div
-        style={{
-          position: "relative",
-          display: "flex",
-          flexDirection: "column",
-          width: "100%",
-          height: "100%",
-          padding: 12,
-        }}
-      >
-        <OgHeader
-          ensAvatar={useravatar}
-          username={username}
-          comments={comments.length}
-        />
-        <OgFooter />
-      </div>
-    </div>,
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     position: "relative",
+    //     overflow: "hidden",
+    //     width: "100%",
+    //     height: "100%",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <OgBackground
+    //     backgroundUrl={
+    //       getFetchableUrl(
+    //         metadata.metadata.image || `${VERCEL_OG}/images/placeholder.png`,
+    //       ) || ""
+    //     }
+    //   />
+    //   <div
+    //     style={{
+    //       position: "relative",
+    //       display: "flex",
+    //       flexDirection: "column",
+    //       width: "100%",
+    //       height: "100%",
+    //       padding: 12,
+    //     }}
+    //   >
+    //     <OgHeader
+    //       ensAvatar={useravatar}
+    //       username={username}
+    //       comments={comments.length}
+    //     />
+    //     <OgFooter />
+    //   </div>
+    // </div>,
+    <OgBackground
+      backgroundUrl={
+        getFetchableUrl(
+          metadata.metadata.image || `${VERCEL_OG}/images/placeholder.png`,
+        ) || ""
+      }
+    />,
     {
       width: WIDTH,
       height: HEIGHT,
