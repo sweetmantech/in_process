6a4144a93978a5cfe27d399d48662083ffff1902
dev <tech.eng.me@gmail.com>
2025-05-07 20:42:31 -0700

    Revert "Create route.ts"
    This reverts commit bff99d216336c00e67a9e40bfe143ed8113169ee.

--- app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  return Response.json(process.env.NEXT_PUBLIC_CROSSMINT_API_KEY as string);
-}
-
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-export const revalidate = 0;
