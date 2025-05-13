a423017e154bdbd64f91450ca89b0e172609cf30
dev <tech.eng.me@gmail.com>
2025-05-13 04:43:24 -0700

    feat: carousel item image loaded sie

---
+++ components/LandingPage/CarouselItem.tsx
@@ -0,0 +1,53 @@
+import { getFetchableUrl } from "@/lib/protocolSdk/ipfs/gateway";
+import { Metadata } from "@/types/token";
+import PdfViewer from "../Renderers/PdfViewer";
+import VideoPlayer from "../Renderers/VideoPlayer";
+import AudioPlayer from "../Renderers/AudioPlayer";
+
+interface CarouselItemProps {
+  metadata: Metadata;
+}
+
+const CarouselItem = ({ metadata }: CarouselItemProps) => {
+  const mimeType = metadata?.content?.mime || "";
+
+  if (mimeType.includes("pdf"))
+    return (
+      <PdfViewer fileUrl={getFetchableUrl(metadata.animation_url) || ""} />
+    );
+  if (mimeType.includes("audio")) {
+    return (
+      <AudioPlayer
+        thumbnailUrl={getFetchableUrl(metadata.image) || ""}
+        audioUrl={getFetchableUrl(metadata.animation_url) || ""}
+      />
+    );
+  }
+  if (mimeType.includes("video"))
+    return (
+      <div className="size-full flex justify-center">
+        <VideoPlayer url={getFetchableUrl(metadata.animation_url) || ""} />
+      </div>
+    );
+  if (mimeType.includes("html"))
+    return (
+      <div className="size-full flex justify-center">
+        <iframe
+          src={getFetchableUrl(metadata.animation_url) || ""}
+          className="w-full"
+        />
+      </div>
+    );
+  return (
+    <div className="grow relative size-full">
+      {/* eslint-disable-next-line */}
+      <img
+        src={getFetchableUrl(metadata.image) || "/images/placeholder.png"}
+        alt="Token Image."
+        className="w-full"
+      />
+    </div>
+  );
+};
+
+export default CarouselItem;
