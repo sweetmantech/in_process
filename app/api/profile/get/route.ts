9ea3d43b7c9e9ee4d6b5de411384cc5146b13096
dev <tech.eng.me@gmail.com>
2025-05-18 19:56:56 -0700

    Update route.ts

--- app/api/profile/get/route.ts
+++ app/api/profile/get/route.ts
@@ -1,20 +1,20 @@
 import getStackClient from "@/lib/stack/getStackClient";
-import getTag from "@/lib/stack/getTag";
+// import getTag from "@/lib/stack/getTag";
 
 export async function GET() {
   try {
     const stackClient = getStackClient();
     const response = await stackClient.getLeaderboard();
-    const users: any = response.leaderboard;
-    const promise = users.map(async (user: any) => {
-      const profile = await getTag(user.address, "profile");
-      return {
-        address: user.address,
-        profile: profile?.length ? profile : null,
-      };
-    });
-    const profiles = await Promise.all(promise);
-    return Response.json(profiles);
+    // const users: any = response.leaderboard;
+    // const promise = users.map(async (user: any) => {
+    //   const profile = await getTag(user.address, "profile");
+    //   return {
+    //     address: user.address,
+    //     profile: profile?.length ? profile : null,
+    //   };
+    // });
+    // const profiles = await Promise.all(promise);
+    return Response.json(response);
   } catch (e: any) {
     console.log(e);
     const message = e?.message ?? "failed to all profiles";
