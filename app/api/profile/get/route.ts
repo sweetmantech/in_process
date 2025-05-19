d8fb7138c279f877fa58ec4d95b43313cbb3af3d
dev <tech.eng.me@gmail.com>
2025-05-18 19:44:56 -0700

    feat: get all profiles

---
+++ app/api/profile/get/route.ts
@@ -0,0 +1,24 @@
+import getStackClient from "@/lib/stack/getStackClient";
+import getTag from "@/lib/stack/getTag";
+
+export async function GET() {
+  try {
+    const stackClient = getStackClient();
+    const response = await stackClient.getLeaderboard();
+    const users: any = response.leaderboard;
+    const promise = users.map(async (user: any) => {
+      const profile = await getTag(user.address, "profile");
+      return profile;
+    });
+    const profiles = await Promise.all(promise);
+    return Response.json(profiles);
+  } catch (e: any) {
+    console.log(e);
+    const message = e?.message ?? "failed to all profiles";
+    return Response.json({ message }, { status: 500 });
+  }
+}
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
