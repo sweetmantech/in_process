7f75ce9ba425cd10ad85d52779dc86c787f04627
dev <tech.eng.me@gmail.com>
2025-05-24 03:45:08 -0700

    integrate submit api

---
+++ hooks/useSubmitFeedback.ts
@@ -0,0 +1,26 @@
+import submitFeedback from "@/lib/submitFeedback";
+import { useState } from "react";
+
+const useSubmitFeedback = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+  const [feedback, setFeedback] = useState<string>("");
+
+  const submit = async () => {
+    setIsLoading(true);
+    submitFeedback(feedback);
+    setIsOpenModal(false);
+    setIsLoading(false);
+  };
+
+  return {
+    submit,
+    isLoading,
+    isOpenModal,
+    setIsOpenModal,
+    setFeedback,
+    feedback,
+  };
+};
+
+export default useSubmitFeedback;
