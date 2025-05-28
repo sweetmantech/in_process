ebb0c55076a50b378a69a93161a5cf426a0bb7f6
sweetman.eth <sweetmantech@gmail.com>
2025-05-28 18:11:39 -0300

    Sweetmantech/myc 2071 landing page metadata (#507)
    * app/.well-known/farcaster.json/route.ts
    
    * landing page metadata
    
    * open text

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ export const revalidate = 300;
 
 export const metadata: Metadata = {
   title: "In Process",
-  description: "Imagined by LATASHÁ",
+  description: "A Collective Onchain Timeline for artists",
   openGraph: {
     title: "In Process",
-    description: "Imagined by LATASHÁ",
+    description: "A Collective Onchain Timeline for artists",
     images: [`${APP_URL}/site_preview.png`],
   },
   other: {
@@ -18,13 +18,12 @@ export const metadata: Metadata = {
       imageUrl: `${APP_URL}/site_preview.png`,
       aspectRatio: "1:1",
       button: {
-        title: "Create",
+        title: "Open",
         action: {
           type: "launch_frame",
-          name: "In Process",
           url: APP_URL,
           splashImageUrl: `${APP_URL}/site_preview.png`,
-          splashBackgroundColor: "#abaaa9",
+          splashBackgroundColor: "#e4e0db",
         },
       },
     }),
