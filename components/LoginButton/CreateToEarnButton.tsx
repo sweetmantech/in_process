6d982217bcd6a9e8cc8199934df005649f1ea001
dev <tech.eng.me@gmail.com>
2025-05-27 23:04:05 -0700

    Update CreateToEarnButton.tsx

--- components/LoginButton/CreateToEarnButton.tsx
+++ components/LoginButton/CreateToEarnButton.tsx
@@ -1,4 +1,4 @@
-import { useEthPriceProvider } from "@/providers/EthPriceProvider";
+import useTotalEarnings from "@/hooks/useTotalEarnings";
 import { useLayoutProvider } from "@/providers/LayoutProvider";
 import { useUserCollectionsProvider } from "@/providers/UserCollectionsProvider";
 import { useRouter } from "next/navigation";
@@ -7,15 +7,10 @@ import { Fragment } from "react";
 const CreateToEarnButton = ({ className = "" }: { className?: string }) => {
   const { push } = useRouter();
   const { toggleNavbar } = useLayoutProvider();
-  const { totalEarnings } = useUserCollectionsProvider();
-  const { ethPrice } = useEthPriceProvider();
-  const ethEarnings = Number(
-    Number(totalEarnings?.eth || 0) * ethPrice,
-  ).toFixed(2);
-  const usdcEarnings = Number(totalEarnings?.usdc).toFixed(2);
-  const totalAmount = parseFloat(ethEarnings) + parseFloat(usdcEarnings);
+  const { totalAmount } = useTotalEarnings();
+  const { isLoading } = useUserCollectionsProvider();
 
-  if (totalAmount > 0 || !totalEarnings) return <Fragment />;
+  if (totalAmount > 0 || isLoading) return <Fragment />;
   return (
     <button
       type="button"
