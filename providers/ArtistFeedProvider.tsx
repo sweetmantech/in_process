b6c824b218a4ba6f94b1dd687b6e9277546a05b7
sweetman.eth <sweetmantech@gmail.com>
2025-06-03 18:19:32 -0300

    Artist Timeline - hide items (even from artist) (#512)

--- providers/ArtistFeedProvider.tsx
+++ providers/ArtistFeedProvider.tsx
@@ -3,7 +3,6 @@
 import useFeeds from "@/hooks/useFeeds";
 import { createContext, useMemo, useContext } from "react";
 import useTimeline from "@/hooks/useTimeline";
-import { useProfileProvider } from "./ProfileProvider";
 import { useCollections } from "@/hooks/useCollections";
 
 const ArtistFeedContext = createContext<
@@ -24,22 +23,19 @@ const ArtistFeedProvider = ({
   artistAddress: string;
 }) => {
   const { hiddenMoments } = useTimeline();
-  const { canEdit } = useProfileProvider();
   const collections = useCollections(artistAddress);
   const feeds = useFeeds(collections.collections || []);
-  const filtered = canEdit
-    ? feeds.feeds
-    : feeds.feeds.filter(
-        (feed) =>
-          !Boolean(
-            hiddenMoments.find(
-              (moment) =>
-                moment.tokenContract.toLowerCase() ===
-                  feed.collection.toLowerCase() &&
-                moment.tokenId === String(feed.tokenId)
-            )
-          )
-      );
+  const filtered = feeds.feeds.filter(
+    (feed) =>
+      !Boolean(
+        hiddenMoments.find(
+          (moment) =>
+            moment.tokenContract.toLowerCase() ===
+              feed.collection.toLowerCase() &&
+            moment.tokenId === String(feed.tokenId)
+        )
+      )
+  );
   const value = useMemo(
     () => ({
       ...feeds,
