a75354cdbf0db79b6376a3aadd1a5f919e81c4a5
Sweets Sweetman <sweetmantech@gmail.com>
2025-06-11 00:10:32 -0300

    import update

--- components/Timeline/TimelineGrid.tsx
+++ components/Timeline/TimelineGrid.tsx
@@ -1,5 +1,5 @@
 import TimelineTable from "@/components/Timeline/Table/TimelineTable";
-import ArtSlider from "@/components/LandingPage/ArtSlider";
+import ArtSlider from "@/components/ArtSlider/ArtSlider";
 
 const TimelineGrid = () => (
   <div className="md:grid md:grid-cols-12 pb-6 gap-10 relative z-30 pt-20 w-full">
