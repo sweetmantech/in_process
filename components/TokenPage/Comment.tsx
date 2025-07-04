b2b2646ea49fe95e3ef3656f4b7148813404591c
true-eye <trueeyedev@gmail.com>
2025-07-03 21:23:07 -0400

    Comment Name - human readable (#578)
    * refactor: fix 0x address with human readable name
    
    * chore: revert useUsername
    
    * chore: update with meaningful name
    
    * refactor: fix type issue
    
    * chore: add debug console
    
    * refactor: update lib function
    
    * chore: remove typep conversion
    
    * refactor: replace with useArtistProfile

--- components/TokenPage/Comment.tsx
+++ components/TokenPage/Comment.tsx
@@ -1,19 +1,28 @@
+import { useArtistProfile } from "@/hooks/useArtistProfile";
 import truncateAddress from "@/lib/truncateAddress";
+import { Address } from "viem";
 
 interface CommentProps {
   comment: string;
-  sender: string;
+  sender: Address;
   timestamp: number;
 }
 
 export const Comment = ({ comment, sender, timestamp }: CommentProps) => {
+  const { data: artistProfile, isLoading, error } = useArtistProfile(sender);
+
+  const truncatedAddress = truncateAddress(sender);
+  const displayName = isLoading
+    ? "Loading..."
+    : error
+      ? truncatedAddress
+      : artistProfile?.username || truncatedAddress;
+
   return (
     <div className="rounded flex items-end justify-between">
       <div>
         <p className="text-base font-spectral tracking-[-1px]">{comment}</p>
-        <p className="text-base font-archivo-medium">
-          {truncateAddress(sender)}
-        </p>
+        <p className="text-base font-archivo-medium">{displayName}</p>
       </div>
       <p className="text-sm font-archivo lowercase">
         {new Date(timestamp).toLocaleString()}
