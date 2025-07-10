68e5f45f56bc37267057817f525591142bd2cbeb
0xSolcroot <de.solcroot@gmail.com>
2025-07-10 13:36:58 -0400

    Create default prices

--- hooks/useMetadataValues.ts
+++ hooks/useMetadataValues.ts
@@ -1,5 +1,5 @@
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const useMetadataValues = () => {
   const pathname = usePathname();
@@ -7,7 +7,7 @@ const useMetadataValues = () => {
 
   const [name, setName] = useState<string>("");
   const [priceUnit, setPriceUnit] = useState<string>(isUsdc ? "usdc" : "eth");
-  const [price, setPrice] = useState("0");
+  const [price, setPrice] = useState("");
   const [description, setDescription] = useState<string>("");
   const [isTimedSale, setIsTimedSale] = useState<boolean>(false);
   const [imageUri, setImageUri] = useState<string>("");
@@ -18,6 +18,14 @@ const useMetadataValues = () => {
   const [isOpenPreviewUpload, setIsOpenPreviewUpload] =
     useState<boolean>(false);
   const [previewSrc, setPreviewSrc] = useState<string>("");
+
+  useEffect(() => {
+    if (priceUnit === "usdc") {
+      setPrice("1");
+    } else {
+      setPrice("0.000111");
+    }
+  }, [priceUnit]);
 
   return {
     name,
