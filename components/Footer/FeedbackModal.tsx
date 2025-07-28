76599430f13cff95dcaae95abd6e1bd9276eb6e2
Sweets Sweetman <sweetmantech@gmail.com>
2025-07-28 14:36:35 -0500

    create component folder for Footer and child components.

--- components/Footer/FeedbackModal.tsx
+++ components/Footer/FeedbackModal.tsx
@@ -4,7 +4,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Label } from "./ui/label";
+import { Label } from "@/components/ui/label";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import useSubmitFeedback from "@/hooks/useSubmitFeedback";
 
