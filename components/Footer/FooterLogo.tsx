7481b1ce688b346084e5894269513ce90a1c7d9b
Sweets Sweetman <sweetmantech@gmail.com>
2025-07-29 10:04:46 -0500

    Footer - topia text changes to row layout (middle align) on desktop

--- components/Footer/FooterLogo.tsx
+++ components/Footer/FooterLogo.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 
 const FooterLogo = () => {
   return (
-    <div className="flex flex-col items-start md:items-center">
-      <Link href="/" aria-label="Go to homepage" className="mb-2 md:mb-0">
+    <div className="flex flex-col md:flex-row items-start md:items-center">
+      <Link
+        href="/"
+        aria-label="Go to homepage"
+        className="mb-2 md:mb-0 md:mr-2"
+      >
         <Image
           src="/footer_logo.svg"
           blurDataURL="/footer_logo.png"
