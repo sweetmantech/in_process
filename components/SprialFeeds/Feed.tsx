a3b1253ee0d56d27902d505212cf7d4d61204c93
a-kurt <atakankurtofficial@gmail.com>
2025-08-09 01:03:33 +0300

    hotfix

--- components/SprialFeeds/Feed.tsx
+++ components/SprialFeeds/Feed.tsx
@@ -31,8 +31,9 @@ const Feed = ({
         onMouseMove={(e) => handleMouseMove(e, feed)}
         onMouseLeave={handleMouseLeave}
         onClick={() => push(`/${feed.creator}`)}
+        dominant-baseline="middle"
       >
-        <tspan fill="#1B1504" fontSize={isMobile ? 3 : 6} textAnchor="middle" dy={isMobile ? "-2" : "-3"}>
+        <tspan fill="#1B1504" fontSize={isMobile ? 3 : 6} textAnchor="middle">
           ⬤
         </tspan>{" "}
         &nbsp;&nbsp;&nbsp;&nbsp;
