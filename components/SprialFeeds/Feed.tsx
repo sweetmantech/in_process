1d01734595415205c00297121a0d7d1145f53798
a-kurt <atakankurtofficial@gmail.com>
2025-08-08 23:57:01 +0300

    dot fixed

--- components/SprialFeeds/Feed.tsx
+++ components/SprialFeeds/Feed.tsx
@@ -32,7 +32,7 @@ const Feed = ({
         onMouseLeave={handleMouseLeave}
         onClick={() => push(`/${feed.creator}`)}
       >
-        <tspan fill="#4E4E4E" fontSize={20}>
+        <tspan fill="#1B1504" fontSize={isMobile ? 3 : 6} textAnchor="middle">
           ⬤
         </tspan>{" "}
         &nbsp;&nbsp;&nbsp;&nbsp;
