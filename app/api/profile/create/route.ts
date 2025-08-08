0c2a065224d9ac950cfd999e0c17dd80fdc2bca7
arunswheller111 <arunswheeler18@gmail.com>
2025-08-08 08:51:19 -0400

    remove stack sdk

--- app/api/profile/create/route.ts
+++ app/api/profile/create/route.ts
@@ -1,29 +1,9 @@
-import deleteTag from "@/lib/stack/deleteTag";
-import setTag from "@/lib/stack/setTag";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json();
-    const { walletAddress, username, bio, instagram, telegram, twitter } = body;
-    await deleteTag(walletAddress, "profile");
-    await setTag(walletAddress, "profile", {
-      username,
-      bio,
-      socials: {
-        instagram,
-        telegram,
-        twitter,
-      },
-    });
-    return Response.json({
-      success: true,
-    });
-  } catch (e: any) {
-    console.log(e);
-    const message = e?.message ?? "failed to create profile";
-    return Response.json({ message }, { status: 500 });
-  }
+  return Response.json({
+    success: true,
+  });
 }
 
 export const dynamic = "force-dynamic";
