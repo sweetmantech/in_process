f2c9f72b2e79794f9c292e63c4765901aa7177b1
a-kurt <atakankurtofficial@gmail.com>
2025-08-09 16:25:15 +0300

    On a moment being able to go back to a creator timeline

--- components/TokenPage/Token.tsx
+++ components/TokenPage/Token.tsx
@@ -7,6 +7,7 @@ import MomentCollected from "./MomentCollected";
 import useIsMobile from "@/hooks/useIsMobile";
 import CommentSection from "./CommentSection";
 import ContentRenderer from "../Renderers";
+import BackToTimeline from "./BackToTimeline";
 
 const Token = () => {
   const { metadata, collected } = useTokenProvider();
@@ -20,9 +21,15 @@ const Token = () => {
           <>
             <div className="grow flex flex-col md:flex-row gap-4 md:gap-10">
               {collected ? (
-                <MomentCollected />
+                <div className="w-full md:max-w-[400px]">
+                  <BackToTimeline />
+                  <MomentCollected />
+                </div>
               ) : (
-                <MetaAndComments commentsHidden={isMobile} />
+                <div className="w-full md:max-w-[400px]">
+                  <BackToTimeline />
+                  <MetaAndComments commentsHidden={isMobile} />
+                </div>
               )}
               <div className="grow w-full flex justify-center">
                 <div className="relative w-full aspect-[576/700] h-fit overflow-hidden font-spectral">
