04bd8cbe6c0e25fef761e3ba31108b8cc4efca85
a-kurt <atakankurtofficial@gmail.com>
2025-08-09 18:39:19 +0300

    fix

--- components/Renderers/Writing.tsx
+++ components/Renderers/Writing.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Skeleton } from "../ui/skeleton";
-import { useMeasure } from "react-use";
 import { cn } from "@/lib/utils";
 
 interface WritingProps {
@@ -10,8 +9,6 @@ interface WritingProps {
 const Writing = ({ fileUrl, description }: WritingProps) => {
   const [text, setText] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [writingRef, { height: contentHeight }] = useMeasure();
-  const [containerRef, { height: containerHeight }] = useMeasure();
 
   useEffect(() => {
     const getText = async () => {
@@ -24,32 +21,24 @@ const Writing = ({ fileUrl, description }: WritingProps) => {
   }, [description, fileUrl]);
 
   if (isLoading) return <Skeleton className="min-h-[200px] size-full" />;
-  const isOverflowed = contentHeight > containerHeight;
-  const shouldCenter = contentHeight < containerHeight;
-  const isShortText = contentHeight < containerHeight / 2;
 
   return (
-    <div
-      className={cn(
-        "size-full relative bg-grey-eggshell text-sm md:text-md",
-        shouldCenter && "flex items-center justify-center",
-        isShortText && "text-xl md:text-3xl",
-      )}
-      ref={containerRef as any}
-    >
-      <div
-        className={cn(
-          "bg-grey-eggshell p-2 !normal-case text-left",
-          !Boolean(containerHeight) && "opacity-0",
-        )}
-        dangerouslySetInnerHTML={{
-          __html: text.replaceAll("\n", "<br/>"),
-        }}
-        ref={writingRef as any}
-      />
-      {isOverflowed && (
-        <div className="h-1/2 absolute size-full left-0 bottom-0 bg-gradientBottomTop" />
-      )}
+    <div className="size-full relative bg-grey-eggshell">
+      <div className="size-full aspect-[571/692] relative">
+        <div className="size-full font-spectral shadow-[5px_6px_2px_2px_#0000000f] border border-[#B8B8B0] bg-white relative">
+          <textarea
+            disabled
+            value={text}
+            className={cn(
+              "relative z-[2] size-full outline-none p-4 bg-grey-eggshell resize-none overflow-y-auto",
+              "text-sm md:text-md"
+            )}
+            readOnly
+          />
+          <div className="pointer-events-none absolute z-[3] left-0 top-0 bg-gradientTopBottom w-full h-24" />
+          <div className="pointer-events-none absolute z-[3] left-0 bottom-0 bg-gradientBottomTop w-full h-24" />
+        </div>
+      </div>
     </div>
   );
 };
