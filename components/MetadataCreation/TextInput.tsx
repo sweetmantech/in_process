384de5a7e15901cd7eae3f2d9a812824656ea477
a-kurt <atakankurtofficial@gmail.com>
2025-08-15 00:37:05 +0300

    fix

--- components/MetadataCreation/TextInput.tsx
+++ components/MetadataCreation/TextInput.tsx
@@ -1,5 +1,5 @@
 import { useZoraCreateProvider } from "@/providers/ZoraCreateProvider";
-import { useState, useEffect } from "react";
+import { ChangeEvent, useState } from "react";
 
 type ScrollPosition = "top" | "mid" | "bottom" | null;
 
@@ -7,8 +7,10 @@ const TextInput = () => {
   const { fileUploading, write, writingText, creating } = useZoraCreateProvider();
   const [scrollPosition, setScrollPosition] = useState<ScrollPosition>(null);
 
-  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
-    const { scrollTop, scrollHeight, clientHeight } = e.target as HTMLDivElement;
+  const [isEditing] = useState(true);
+
+  const handleScroll = (e: React.UIEvent<HTMLElement>) => {
+    const { scrollTop, scrollHeight, clientHeight } = e.target as HTMLElement;
     const position: ScrollPosition =
       scrollTop === 0
         ? "top"
@@ -18,33 +20,37 @@ const TextInput = () => {
     setScrollPosition(position);
   };
 
-  useEffect(() => {
-    // initialize scroll position on mount
-    setScrollPosition("top");
-  }, []);
-
   return (
-    <div className="w-full min-h-[300px] md:min-h-auto md:aspect-[571/692] relative bg-contain">
-      <div className="size-full">
-        <div className="size-full !font-spectral shadow-[5px_6px_2px_2px_#0000000f] border border-grey-moss-300 bg-white disabled:cursor-not-allowed relative">
-          <div
-            className="relative z-[2] size-full p-2 md:p-4 pt-24 bg-grey-eggshell overflow-y-auto whitespace-pre-wrap"
-            contentEditable={!fileUploading && !creating}
-            suppressContentEditableWarning
-            onInput={(e) => write((e.target as HTMLDivElement).innerText)}
-            onScroll={handleScroll}
-          >
-            {writingText}
-          </div>
+    <div className="overflow-hidden size-full !font-spectral shadow-[5px_6px_2px_2px_#0000000f] border border-grey-moss-300 bg-white disabled:cursor-not-allowed relative">
+      {isEditing ? (
+        <textarea
+          className="relative z-[2] size-full !outline-none p-2 md:p-4 pt-24 bg-grey-eggshell !resize-none overflow-y-auto"
+          value={writingText}
+          disabled={Boolean(fileUploading || creating)}
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
+            write(e.target.value);
+          }}
+          onScroll={handleScroll}
+        />
+      ) : (
+        <div
+          className="relative z-[2] size-full p-2 md:p-4 pt-24 bg-grey-eggshell overflow-y-auto whitespace-pre-wrap"
+          onScroll={handleScroll}
+        >
+          {writingText}
+        </div>
+      )}
 
+      {scrollPosition && (
+        <>
           {scrollPosition !== "top" && (
             <div className="pointer-events-none absolute z-[3] left-0 top-0 bg-gradientTopBottom w-full h-24" />
           )}
           {scrollPosition !== "bottom" && (
             <div className="pointer-events-none absolute z-[3] left-0 bottom-0 bg-gradientBottomTop w-full h-24" />
           )}
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
