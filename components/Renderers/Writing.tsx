cc3bd5d0a25e60deca7a30a730ac40070159ce01
a-kurt <atakankurtofficial@gmail.com>
2025-08-17 16:44:47 +0300

    fix

--- components/Renderers/Writing.tsx
+++ components/Renderers/Writing.tsx
@@ -1,17 +1,15 @@
 import { useEffect, useState } from "react";
 import { Skeleton } from "../ui/skeleton";
-import { useMeasure } from "react-use";
-import { cn } from "@/lib/utils";
 
 interface WritingProps {
   fileUrl: string;
   description: string;
 }
+
 const Writing = ({ fileUrl, description }: WritingProps) => {
   const [text, setText] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [writingRef, { height: contentHeight }] = useMeasure();
-  const [containerRef, { height: containerHeight }] = useMeasure();
+  const [scrollPosition, setScrollPosition] = useState<"top" | "mid" | "bottom">("top");
 
   useEffect(() => {
     const getText = async () => {
@@ -23,32 +21,33 @@ const Writing = ({ fileUrl, description }: WritingProps) => {
     if (fileUrl) getText();
   }, [description, fileUrl]);
 
+  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const { scrollTop, scrollHeight, clientHeight } = e.target as HTMLDivElement;
+    const position =
+      scrollTop === 0
+        ? "top"
+        : scrollHeight - scrollTop - clientHeight <= 5
+        ? "bottom"
+        : "mid";
+    setScrollPosition(position);
+  };
+
   if (isLoading) return <Skeleton className="min-h-[200px] size-full" />;
-  const isOverflowed = contentHeight > containerHeight;
-  const shouldCenter = contentHeight < containerHeight;
-  const isShortText = contentHeight < containerHeight / 2;
 
   return (
-    <div
-      className={cn(
-        "size-full relative bg-grey-eggshell text-sm md:text-md",
-        shouldCenter && "flex items-center justify-center",
-        isShortText && "text-xl md:text-3xl",
-      )}
-      ref={containerRef as any}
-    >
+    <div className="size-full !font-spectral shadow-[5px_6px_2px_2px_#0000000f] border border-grey-moss-300 bg-white relative">
       <div
-        className={cn(
-          "bg-grey-eggshell p-2 !normal-case text-left",
-          !Boolean(containerHeight) && "opacity-0",
-        )}
+        className="relative z-[2] size-full p-2 md:p-4 pt-24 bg-grey-eggshell overflow-y-auto whitespace-pre-wrap text-sm md:text-base"
+        onScroll={handleScroll}
         dangerouslySetInnerHTML={{
           __html: text.replaceAll("\n", "<br/>"),
         }}
-        ref={writingRef as any}
       />
-      {isOverflowed && (
-        <div className="h-1/2 absolute size-full left-0 bottom-0 bg-gradientBottomTop" />
+      {scrollPosition !== "top" && (
+        <div className="pointer-events-none absolute z-[3] left-0 top-0 bg-gradientTopBottom w-full h-24" />
+      )}
+      {scrollPosition !== "bottom" && (
+        <div className="pointer-events-none absolute z-[3] left-0 bottom-0 bg-gradientBottomTop w-full h-24" />
       )}
     </div>
   );
