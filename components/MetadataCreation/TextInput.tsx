61c37ab8cc02fdec2c28fdb1782c46d6273e40df
a-kurt <atakankurtofficial@gmail.com>
2025-08-17 17:56:15 +0300

    rabbit fix

--- components/MetadataCreation/TextInput.tsx
+++ components/MetadataCreation/TextInput.tsx
@@ -1,5 +1,5 @@
 import { useZoraCreateProvider } from "@/providers/ZoraCreateProvider";
-import { ChangeEvent, useState, useEffect } from "react";
+import { ChangeEvent, UIEvent, useState, useEffect } from "react";
 
 type ScrollPosition = "top" | "mid" | "bottom" | null;
 
@@ -8,9 +8,8 @@ const TextInput = () => {
     useZoraCreateProvider();
   const [scrollPosition, setScrollPosition] = useState<ScrollPosition>(null);
 
-  const handleScroll = (e: React.UIEvent<HTMLTextAreaElement | HTMLDivElement>) => {
-    const { scrollTop, scrollHeight, clientHeight } =
-      e.target as HTMLTextAreaElement | HTMLDivElement;
+  const handleScroll = (e: UIEvent<HTMLTextAreaElement | HTMLDivElement>) => {
+    const { scrollTop, scrollHeight, clientHeight } = e.currentTarget;
     const position: ScrollPosition =
       scrollTop === 0
         ? "top"
@@ -30,12 +29,21 @@ const TextInput = () => {
     return (
       <div className="size-full !font-spectral shadow-[5px_6px_2px_2px_#0000000f] border border-grey-moss-300 bg-white disabled:cursor-not-allowed relative">
         <div
-          className="relative z-[2] size-full p-2 md:p-4 pt-24 bg-grey-eggshell overflow-y-auto whitespace-pre-wrap"
+          className="relative z-[2] size-full p-2 md:p-4 pt-24 pb-24 bg-grey-eggshell overflow-y-auto whitespace-pre-wrap break-words"
           onScroll={handleScroll}
         >
           {writingText}
         </div>
-        <div className="pointer-events-none absolute z-[3] left-0 top-0 bg-gradientTopBottom w-full h-24" />
+        {scrollPosition && (
+          <>
+            {scrollPosition !== "top" && (
+              <div className="pointer-events-none absolute z-[3] left-0 top-0 bg-gradientTopBottom w-full h-24" />
+            )}
+            {scrollPosition !== "bottom" && (
+              <div className="pointer-events-none absolute z-[3] left-0 bottom-0 bg-gradientBottomTop w-full h-24" />
+            )}
+          </>
+        )}
       </div>
     );
   }
