7aaf585f131af4b9c2d9032117320ffbae3679d7
Sweets Sweetman <sweetmantech@gmail.com>
2025-08-18 10:39:23 -0500

    Create button - toast missing metadata

--- components/CreateForm/CreateButton.tsx
+++ components/CreateForm/CreateButton.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { useZoraCreateProvider } from "@/providers/ZoraCreateProvider";
+import { toast } from "sonner";
 
 const CreateButton = () => {
   const {
@@ -20,11 +21,25 @@ const CreateButton = () => {
     !creating &&
       name &&
       (previewUri || writingText) &&
-      Boolean(animationUri || link || embedCode || imageUri || writingText),
+      Boolean(animationUri || link || embedCode || imageUri || writingText)
   );
+
+  const toastCreateError = () => {
+    if (!previewUri && !writingText) {
+      toast.error("Missing a preview image");
+    } else if (!animationUri && !link && !embedCode && !imageUri) {
+      toast.error("Missing media");
+    } else {
+      toast.error("Error creating");
+    }
+  };
 
   const handleCreate = async () => {
     try {
+      if (!canCreate) {
+        toastCreateError();
+        return;
+      }
       await create();
     } catch (error) {
       console.error("Error creating:", error);
@@ -34,7 +49,7 @@ const CreateButton = () => {
   return (
     <Button
       onClick={handleCreate}
-      disabled={!canCreate}
+      disabled={creating}
       className="self-center w-fit z-10 px-14 py-5 md:py-6 md:w-full md:!mt-4 !font-archivo bg-black hover:bg-grey-moss-300 text-grey-eggshell md:h-[60px] !text-xl !rounded-sm transform transition-transform duration-150 disabled:opacity-1 disabled:!cursor-not-allowed disabled:!pointer-events-auto"
     >
       {creating ? "creating..." : "create"}
