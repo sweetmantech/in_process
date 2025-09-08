364890c6a8b1b46e165293eb612391c9fe86b6cf
sweetman.eth <sweetmantech@gmail.com>
2025-09-08 13:23:13 -0300

    High Priority - Implement New Header Background (#647)

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -19,29 +19,33 @@ const Header = () => {
 
   return (
     <div
-      className={`${isOpenNavbar ? "bg-grey-moss-900 border-b border-grey-moss-900" : ""} md:border-none md:bg-transparent 
-      z-[9999999] sticky top-0 w-screen flex justify-between items-center px-6 md:px-10 py-4 md:pt-6`}
+      className={`${isOpenNavbar ? "bg-grey-moss-900" : "bg-grey-moss-100/90"} md:bg-grey-moss-100/90 opacity-99 
+      z-[9999999] sticky top-0 w-screen`}
     >
-      <Logo />
-      <div className="flex items-center gap-1 md:gap-2" ref={menuRef}>
-        <ArtistSearch />
-        {!totalAmount && !isMobile && <CreateCTAButton />}
-        <div className="md:relative flex items-center gap-1 md:gap-2">
-          {!isExpandedSearchInput && <LoginButton />}
-          {signedAddress && (
-            <button
-              onClick={toggleNavbar}
-              type="button"
-              className="block md:hidden flex flex-col bg-grey-moss-400 py-1.5 px-2 rounded-md"
-            >
-              <div className="size-2 rounded-full bg-grey-moss-100" />
-              <div className="size-2 rounded-full bg-grey-moss-100" />
-              <div className="size-2 rounded-full bg-grey-moss-100" />
-            </button>
-          )}
-          {isOpenNavbar && signedAddress && <DropdownMenu />}
+      <div className="flex justify-between items-center px-6 md:px-10 py-8">
+        <Logo />
+        <div className="flex items-center gap-1 md:gap-2" ref={menuRef}>
+          <ArtistSearch />
+          {!totalAmount && !isMobile && <CreateCTAButton />}
+          <div className="md:relative flex items-center gap-1 md:gap-2">
+            {!isExpandedSearchInput && <LoginButton />}
+            {signedAddress && (
+              <button
+                onClick={toggleNavbar}
+                type="button"
+                className="block md:hidden flex flex-col bg-grey-moss-400 py-1.5 px-2 rounded-md"
+              >
+                <div className="size-2 rounded-full bg-grey-moss-100" />
+                <div className="size-2 rounded-full bg-grey-moss-100" />
+                <div className="size-2 rounded-full bg-grey-moss-100" />
+              </button>
+            )}
+            {isOpenNavbar && signedAddress && <DropdownMenu />}
+          </div>
         </div>
       </div>
+      {/* Black line from logo to wallet dropdown */}
+      <div className="border-b border-grey mx-6 md:mx-10" />
     </div>
   );
 };
