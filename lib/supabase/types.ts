17102204a559611347854ae18d6af0edd247f66a
sweetman.eth <sweetmantech@gmail.com>
2025-09-16 11:08:28 -0300

    Sweetmantech/myc 2901 supabase in process payments new table (#651)
    * Telegram - fix missing bot messages
    
    * supabase - in_process_payments - new table

--- lib/supabase/types.ts
+++ lib/supabase/types.ts
@@ -4,111 +4,168 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "12.2.3 (519615d)";
+  };
   public: {
     Tables: {
       in_process_artists: {
         Row: {
-          address: string
-          bio: string | null
-          instagram_username: string | null
-          telegram_username: string | null
-          twitter_username: string | null
-          username: string | null
-        }
+          address: string;
+          bio: string | null;
+          instagram_username: string | null;
+          telegram_username: string | null;
+          twitter_username: string | null;
+          username: string | null;
+        };
         Insert: {
-          address: string
-          bio?: string | null
-          instagram_username?: string | null
-          telegram_username?: string | null
-          twitter_username?: string | null
-          username?: string | null
-        }
+          address: string;
+          bio?: string | null;
+          instagram_username?: string | null;
+          telegram_username?: string | null;
+          twitter_username?: string | null;
+          username?: string | null;
+        };
         Update: {
-          address?: string
-          bio?: string | null
-          instagram_username?: string | null
-          telegram_username?: string | null
-          twitter_username?: string | null
-          username?: string | null
-        }
-        Relationships: []
-      }
-      in_process_tokens: {
+          address?: string;
+          bio?: string | null;
+          instagram_username?: string | null;
+          telegram_username?: string | null;
+          twitter_username?: string | null;
+          username?: string | null;
+        };
+        Relationships: [];
+      };
+      in_process_payments: {
         Row: {
-          address: string
-          chainId: number
-          createdAt: string
-          defaultAdmin: string
-          hidden: boolean
-          id: string
-          tokenId: number
-          uri: string
-        }
+          amount: number | null;
+          block: number | null;
+          buyer: string | null;
+          hash: string | null;
+          id: number;
+          quantity: number | null;
+          token: string;
+        };
         Insert: {
-          address?: string
-          chainId: number
-          createdAt: string
-          defaultAdmin?: string
-          hidden?: boolean
-          id?: string
-          tokenId: number
-          uri?: string
-        }
+          amount?: number | null;
+          block?: number | null;
+          buyer?: string | null;
+          hash?: string | null;
+          id?: number;
+          quantity?: number | null;
+          token?: string;
+        };
         Update: {
-          address?: string
-          chainId?: number
-          createdAt?: string
-          defaultAdmin?: string
-          hidden?: boolean
-          id?: string
-          tokenId?: number
-          uri?: string
-        }
+          amount?: number | null;
+          block?: number | null;
+          buyer?: string | null;
+          hash?: string | null;
+          id?: number;
+          quantity?: number | null;
+          token?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "fk_defaultadmin_artist"
-            columns: ["defaultAdmin"]
-            isOneToOne: false
-            referencedRelation: "in_process_artists"
-            referencedColumns: ["address"]
+            foreignKeyName: "in_process_payments_buyer_fkey";
+            columns: ["buyer"];
+            isOneToOne: false;
+            referencedRelation: "in_process_artists";
+            referencedColumns: ["address"];
           },
-        ]
-      }
-    }
+          {
+            foreignKeyName: "in_process_payments_token_fkey";
+            columns: ["token"];
+            isOneToOne: false;
+            referencedRelation: "in_process_tokens";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      in_process_tokens: {
+        Row: {
+          address: string;
+          chainId: number;
+          createdAt: string;
+          defaultAdmin: string;
+          hidden: boolean;
+          id: string;
+          tokenId: number;
+          uri: string;
+        };
+        Insert: {
+          address?: string;
+          chainId: number;
+          createdAt: string;
+          defaultAdmin?: string;
+          hidden?: boolean;
+          id?: string;
+          tokenId: number;
+          uri?: string;
+        };
+        Update: {
+          address?: string;
+          chainId?: number;
+          createdAt?: string;
+          defaultAdmin?: string;
+          hidden?: boolean;
+          id?: string;
+          tokenId?: number;
+          uri?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "fk_defaultadmin_artist";
+            columns: ["defaultAdmin"];
+            isOneToOne: false;
+            referencedRelation: "in_process_artists";
+            referencedColumns: ["address"];
+          },
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type DefaultSchema = Database[Extract<keyof Database, "public">]
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">;
+
+type DefaultSchema = DatabaseWithoutInternals[Extract<
+  keyof Database,
+  "public"
+>];
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
     : never = never,
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R;
     }
     ? R
     : never
@@ -116,90 +173,98 @@ export type Tables<
         DefaultSchema["Views"])
     ? (DefaultSchema["Tables"] &
         DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I;
     }
     ? I
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U;
     }
     ? U
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
-> = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+> = DefaultSchemaEnumNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
     ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never
+    : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
-    | { schema: keyof Database },
+    | { schema: keyof DatabaseWithoutInternals },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof DatabaseWithoutInternals;
   }
-    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
-> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+> = PublicCompositeTypeNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
     ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+    : never;
 
 export const Constants = {
   public: {
     Enums: {},
   },
-} as const
+} as const;
