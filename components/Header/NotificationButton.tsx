94ef06c9d4d97621331bee0fe7e05b83e11fa3c1
sweetman.eth <sweetmantech@gmail.com>
2025-09-22 22:06:02 -0300

    NavBarBell - show badge with number of notifications which have not b… (#669)
    * NavBarBell - show badge with number of notifications which have not been viewed.
    
    * Open Closed Principle - NotificationCountBadge.tsx
    
    * remove redundant dev comments.

--- components/Header/NotificationButton.tsx
+++ components/Header/NotificationButton.tsx
@@ -2,6 +2,7 @@
 
 import { BellIcon } from "@radix-ui/react-icons";
 import { useRouter } from "next/navigation";
+import NotificationCountBadge from "./NotificationCountBadge";
 
 const NotificationButton = () => {
   const router = useRouter();
@@ -10,10 +11,11 @@ const NotificationButton = () => {
     <button
       onClick={() => router.push("/notifications")}
       type="button"
-      className="p-2 rounded-md hover:bg-grey-moss-200/50 transition-colors"
+      className="relative p-2 rounded-md hover:bg-grey-moss-200/50 transition-colors"
       aria-label="View notifications"
     >
       <BellIcon className="w-5 h-5 text-grey-moss-300" />
+      <NotificationCountBadge />
     </button>
   );
 };
