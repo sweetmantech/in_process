6b561b751defec92c51fe105660b38a870f1ecb7
thomas jin <tech.eng.me@gmail.com>
2025-10-07 05:52:38 +1000

    Update route.ts

--- app/api/profile/create/route.ts
+++ app/api/profile/create/route.ts
@@ -1,11 +1,31 @@
+import { upsertProfile } from "@/lib/supabase/in_process_artists/upsertProfile";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    const { walletAddress, username, bio, instagram, telegram, twitter } = body;
+    const {
+      address,
+      username,
+      bio,
+      instagram_username,
+      twitter_username,
+      farcaster_username,
+      telegram_username,
+    } = body;
+    const { data, error } = await upsertProfile({
+      address: address.toLowerCase(),
+      username,
+      bio,
+      twitter_username,
+      instagram_username,
+      farcaster_username,
+      telegram_username,
+    });
+    if (error) throw new Error();
     return Response.json({
       success: true,
+      data,
     });
   } catch (e: any) {
     console.log(e);
