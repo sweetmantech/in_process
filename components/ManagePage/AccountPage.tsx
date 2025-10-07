05b833983066684923cbb32cf2074cff5b6a2fc2
thomas jin <tech.eng.me@gmail.com>
2025-10-07 21:34:11 +1000

    save profile functionality

--- components/ManagePage/AccountPage.tsx
+++ components/ManagePage/AccountPage.tsx
@@ -3,12 +3,43 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { saveIndentify } from "@/hooks/useProfile";
 import { useProfileProvider } from "@/providers/ProfileProvider";
+import { useUserProvider } from "@/providers/UserProvider";
+import { useState } from "react";
 
 const AccountPage = () => {
-  const { twitter, instagram, farcaster, username, bio } = useProfileProvider();
+  const {
+    twitter,
+    instagram,
+    farcaster,
+    username,
+    bio,
+    telegram,
+    setBio,
+    setTwitter,
+    setInstagram,
+    setFarcaster,
+    setUserName,
+  } = useProfileProvider();
+  const { connectedAddress } = useUserProvider();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const onSave = async () => {
+    setIsLoading(true);
+    await saveIndentify({
+      address: connectedAddress as string,
+      username,
+      bio,
+      farcaster_username: farcaster,
+      twitter_username: twitter,
+      instagram_username: instagram,
+      telegram_username: telegram,
+    });
+    setIsLoading(false);
+  };
+
   return (
-    <main className="flex flex-col gap-6 md:gap-24 font-archivo">
+    <main className="flex flex-col font-archivo">
       <section className="grid grid-cols-12 gap-1">
         <p className="text-base col-span-12 md:col-span-2">your info</p>
         <div className="col-span-12 md:col-span-10 flex flex-col gap-4">
@@ -18,6 +49,7 @@ const AccountPage = () => {
               placeholder="ex: ziad.eth"
               className="resize-none font-spectral mt-1"
               value={username}
+              onChange={(e) => setUserName(e.target.value)}
             />
           </fieldset>
           <fieldset>
@@ -27,11 +59,12 @@ const AccountPage = () => {
               minRows={7}
               className="resize-none font-spectral mt-1"
               value={bio}
+              onChange={(e) => setBio(e.target.value)}
             />
           </fieldset>
         </div>
       </section>
-      <section className="grid grid-cols-12 gap-1">
+      <section className="grid grid-cols-12 gap-1 mt-6 md:mt-24">
         <p className="text-base col-span-12 md:col-span-2">
           connected <br className="hidden md:block" />
           accounts
@@ -43,6 +76,7 @@ const AccountPage = () => {
               placeholder="ex: https://instagram.com/helly"
               className="resize-none font-spectral mt-1"
               value={instagram}
+              onChange={(e) => setInstagram(e.target.value)}
             />
           </fieldset>
           <fieldset>
@@ -51,6 +85,7 @@ const AccountPage = () => {
               placeholder="ex: https://x.com/helly"
               className="resize-none font-spectral mt-1"
               value={twitter}
+              onChange={(e) => setTwitter(e.target.value)}
             />
           </fieldset>
           <fieldset>
@@ -59,10 +94,17 @@ const AccountPage = () => {
               placeholder="ex: https://farcaster.com/helly"
               className="resize-none font-spectral mt-1"
               value={farcaster}
+              onChange={(e) => setFarcaster(e.target.value)}
             />
           </fieldset>
         </div>
       </section>
+      <button
+        className="mt-4 mr-4 self-end px-4 py-2 rounded-md flex items-center gap-2 bg-grey-moss-900 font-archivo text-grey-eggshell hover:bg-grey-eggshell hover:text-grey-moss-900"
+        onClick={onSave}
+      >
+        {isLoading ? "saving..." : "save"}
+      </button>
     </main>
   );
 };
