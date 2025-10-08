46f52d66d4a0345eaa0a7981677f4d859df87fde
thomas jin <tech.eng.me@gmail.com>
2025-10-09 01:17:35 +1000

    fix feedback

--- lib/supabase/migrations/20251008151505_in_process_artist_smart_wallets.sql
+++ lib/supabase/migrations/20251008151505_in_process_artist_smart_wallets.sql
@@ -5,6 +5,8 @@ create table "public"."in_process_artist_smart_wallets" (
 );
 
 
+alter table "public"."in_process_artist_smart_wallets" enable row level security;
+
 CREATE UNIQUE INDEX in_process_artist_smart_wallets_pkey ON public.in_process_artist_smart_wallets USING btree (smart_wallet_address);
 
 alter table "public"."in_process_artist_smart_wallets" add constraint "in_process_artist_smart_wallets_pkey" PRIMARY KEY using index "in_process_artist_smart_wallets_pkey";
@@ -12,5 +14,3 @@ alter table "public"."in_process_artist_smart_wallets" add constraint "in_proces
 alter table "public"."in_process_artist_smart_wallets" add constraint "in_process_artist_smart_wallets_artist_address_fkey" FOREIGN KEY (artist_address) REFERENCES in_process_artists(address) ON UPDATE CASCADE ON DELETE CASCADE not valid;
 
 alter table "public"."in_process_artist_smart_wallets" validate constraint "in_process_artist_smart_wallets_artist_address_fkey";
-
-
