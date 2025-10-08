a4550701e8ce9735c1a09ca84fe18d71d159a1b6
thomas jin <tech.eng.me@gmail.com>
2025-10-09 04:35:42 +1000

    WIP

--- components/ManagePage/AccountPage.tsx
+++ components/ManagePage/AccountPage.tsx
@@ -7,7 +7,9 @@ import { saveIndentify } from "@/hooks/useProfile";
 import { useProfileProvider } from "@/providers/ProfileProvider";
 import { useUserProvider } from "@/providers/UserProvider";
 import { useState } from "react";
-import SearchModal from "../SearchModal/SearchModal";
+import { useConnectWallet } from "@privy-io/react-auth";
+import fetchArtistProfile from "@/lib/fetchArtistProfile";
+import { Address } from "viem";
 
 const AccountPage = () => {
   const {
@@ -23,7 +25,19 @@ const AccountPage = () => {
     setFarcaster,
     setUserName,
   } = useProfileProvider();
-  const { connectedAddress } = useUserProvider();
+  const { connectedAddress, email } = useUserProvider();
+  const { connectWallet } = useConnectWallet({
+    onSuccess: async ({ wallet }) => {
+      const profile = await fetchArtistProfile(wallet.address as Address);
+      if (profile.storage !== "supabase") {
+        await saveIndentify({
+          ...profile,
+          address: wallet.address as Address,
+        })
+      }
+      
+    },
+  });
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const onSave = async () => {
     setIsLoading(true);
@@ -101,7 +115,12 @@ const AccountPage = () => {
         </div>
       </section>
       <section className="flex justify-end gap-3 mt-4">
-        <SearchModal />
+        {email && connectedAddress && <button
+          onClick={connectWallet}
+          className="self-end px-4 py-2 rounded-md flex items-center gap-2 bg-grey-moss-900 font-archivo text-grey-eggshell hover:bg-grey-eggshell hover:text-grey-moss-900"
+        >
+          connect
+        </button>}
         <button
           className="mr-4 self-end px-4 py-2 rounded-md flex items-center gap-2 bg-grey-moss-900 font-archivo text-grey-eggshell hover:bg-grey-eggshell hover:text-grey-moss-900"
           onClick={onSave}
