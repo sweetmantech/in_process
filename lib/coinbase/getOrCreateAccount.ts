e89062f2b29eee2395e3413815909a3b473c86bb
thomas jin <tech.eng.me@gmail.com>
2025-10-13 18:15:23 +1000

    fix random account

--- lib/coinbase/getOrCreateAccount.ts
+++ lib/coinbase/getOrCreateAccount.ts
@@ -1,5 +1,5 @@
 import cdp from "@/lib/coinbase/client";
-import { EvmServerAccount } from "@coinbase/cdp-sdk";
+import { EvmSmartAccount } from "@coinbase/cdp-sdk";
 import { type Address } from "viem";
 import { deterministicAccountName } from "./deterministricAccountName";
 
@@ -7,10 +7,15 @@ export async function getOrCreateAccount({
   address,
 }: {
   address: Address;
-}): Promise<EvmServerAccount> {
-  const smartWallet = await cdp.evm.getOrCreateAccount({
+}): Promise<EvmSmartAccount> {
+  const evmAccount = await cdp.evm.getOrCreateAccount({
     name: deterministicAccountName(address),
   });
 
-  return smartWallet;
+  const smartAccount = await cdp.evm.getSmartAccount({
+    address: evmAccount.address,
+    owner: evmAccount
+  })
+
+  return smartAccount;
 }
