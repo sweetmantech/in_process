163815a72139664775fd50e967fd36f233bf2a62
thomas jin <tech.eng.me@gmail.com>
2025-10-15 08:23:50 +1000

    DELETE: disconnect a smart wallet

--- lib/supabase/in_process_artist_social_wallets/removeSocialWallet.ts
+++ lib/supabase/in_process_artist_social_wallets/removeSocialWallet.ts
@@ -1,14 +1,17 @@
 import { supabase } from "@/lib/supabase/client";
 import type { Database } from "@/lib/supabase/types";
 
-export async function insertSocialWallet({
+export async function removeSocialWallet({
   artist_address,
   social_wallet,
 }: Database["public"]["Tables"]["in_process_artist_social_wallets"]["Insert"]) {
-  const { error } = await supabase.from("in_process_artist_social_wallets").insert({
-    artist_address,
-    social_wallet,
-  });
+  const { data, error } = await supabase
+    .from("in_process_artist_social_wallets")
+    .delete()
+    .eq("artist_address", artist_address)
+    .eq("social_wallet", social_wallet)
+    .select()
+    .single();
 
   if (error) return { error };
 
