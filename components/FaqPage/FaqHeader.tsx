ac4ed4714ac8313f5c5c515cef2a6dd789773c66
thomas jin <tech.eng.me@gmail.com>
2025-10-15 18:43:19 +1000

    in process -> in•process

--- components/FaqPage/FaqHeader.tsx
+++ components/FaqPage/FaqHeader.tsx
@@ -3,7 +3,7 @@ const FaqHeader = () => {
   return (
     <div className="flex flex-col md:flex-row md:items-start md:justify-between mb-6 md:mb-16">
       <h1 className="font-archivo lowercase relative z-[2] text-[24px] leading-[110%] md:text-[48px] md:leading-[100%] font-medium tracking-[-0.05em] text-[#1b1504]">
-        in process: faq
+        in•process: faq
       </h1>
       <div className="font-spectral italic lowercase text-left md:text-right mt-4 text-[16px] leading-[100%] md:text-[24px] font-medium tracking-[-0.05em] text-[#1b1504] md:text-[#4e4e4e]">
         a collective timeline for creative culture
