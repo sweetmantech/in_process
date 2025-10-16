399a9a24e2efd40cade8e51a91b31d6be088ab4c
thomas jin <tech.eng.me@gmail.com>
2025-10-16 21:41:50 +1000

    fix redirect bug

--- components/LoginButton/DropdownMenu.tsx
+++ components/LoginButton/DropdownMenu.tsx
@@ -1,17 +1,17 @@
-import useSignedAddress from "@/hooks/useSignedAddress";
 import { useFrameProvider } from "@/providers/FrameProvider";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Divider from "./Divider";
 import { useLayoutProvider } from "@/providers/LayoutProvider";
 import { usePrivy } from "@privy-io/react-auth";
+import { useUserProvider } from "@/providers/UserProvider";
 
 export function DropdownMenu() {
   const { context } = useFrameProvider();
   const { push } = useRouter();
-  const signedAddress = useSignedAddress();
   const { toggleNavbar } = useLayoutProvider();
   const { logout } = usePrivy();
+  const { artistWallet, connectedAddress } = useUserProvider();
 
   return (
     <div className="absolute top-full left-0 right-0 h-screen md:h-fit bg-grey-moss-900 shadow-lg font-archivo z-[999999999] rounded-b-sm border-t-0">
@@ -19,7 +19,7 @@ export function DropdownMenu() {
       <button
         onClick={() => {
           toggleNavbar();
-          push(`/${signedAddress}`);
+          push(`/${artistWallet || connectedAddress}`);
         }}
         className="w-full text-left pl-14 md:px-4 py-4 md:py-2 text-white text-2xl md:text-base hover:bg-[#333333] hover:rounded-b-sm transition-colors"
       >
