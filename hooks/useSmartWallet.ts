19869a90f818fbae274b1ab00b88d928b863bfc6
thomas jin <tech.eng.me@gmail.com>
2025-10-21 03:11:16 +1000

    fix flip bug

--- hooks/useSmartWallet.ts
+++ hooks/useSmartWallet.ts
@@ -6,22 +6,23 @@ import { Address } from "viem";
 
 const useSmartWallet = () => {
   const [smartWallet, setSmartWallet] = useState<string>("");
-  const { artistWallet, connectedAddress } = useUserProvider();
+  const { artistWallet } = useUserProvider();
   const [balance, setBalance] = useState<string>("0.00");
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const fetchSmartWallet = useCallback(async () => {
-    const address = artistWallet || connectedAddress;
-    if (!address) return;
-    const smartWallet = await getSmartWallet(address);
+    if (!artistWallet) return;
+    const smartWallet = await getSmartWallet(artistWallet);
     setSmartWallet(smartWallet as Address);
     const balance = await getBalance(smartWallet);
     setBalance(balance);
     setIsLoading(false);
-  }, [artistWallet, connectedAddress]);
+  }, [artistWallet]);
 
   useEffect(() => {
-    fetchSmartWallet();
+    setTimeout(() => {
+      fetchSmartWallet();
+    }, 1000);
   }, [fetchSmartWallet]);
 
   return {
