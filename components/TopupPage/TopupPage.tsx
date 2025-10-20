30077ba225350c06a0383260b289eee0d7885539
thomas jin <tech.eng.me@gmail.com>
2025-10-20 13:08:15 +1000

    signToInProcess

--- components/TopupPage/TopupPage.tsx
+++ components/TopupPage/TopupPage.tsx
@@ -3,8 +3,26 @@
 import { Balance } from "./Balance";
 import { Wallet } from "./Wallet";
 import { Deposit } from "./Deposit";
+import { useUserProvider } from "@/providers/UserProvider";
+import SignToInProcess from "../ManagePage/SignToInProcess";
+import { Fragment, useEffect, useState } from "react";
+import { usePrivy } from "@privy-io/react-auth";
 
 const TopupPage = () => {
+  const { connectedAddress } = useUserProvider();
+  const [loaded, setLoaded] = useState<boolean>(false);
+  const { ready } = usePrivy();
+
+  useEffect(() => {
+    if (ready)
+      setTimeout(() => {
+        setLoaded(true);
+      }, 1000);
+  }, [ready]);
+
+  if (!loaded) return <Fragment />;
+  if (!connectedAddress) return <SignToInProcess />;
+
   return (
     <main className="min-h-screen p-4 md:p-8">
       <div className="mx-auto max-w-4xl space-y-6">
