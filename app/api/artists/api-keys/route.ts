ee49f1ac33462ea5cdb2b3b12215112a39a7e093
thomas jin <tech.eng.me@gmail.com>
2025-10-24 05:53:59 +1000

    Update route.ts

--- app/api/artists/api-keys/route.ts
+++ app/api/artists/api-keys/route.ts
@@ -43,7 +43,14 @@ export async function POST(req: NextRequest) {
     });
   } catch (e: any) {
     console.log(e);
-    const message = e?.message ?? "failed to create an api key";
-    return Response.json({ message }, { status: 500 });
+    // const message = e?.message ?? "failed to create an api key";
+    return Response.json(
+      {
+        appId: process.env.NEXT_PUBLIC_PRIVY_APP_ID,
+        appSecret: process.env.PRIVY_API_KEY,
+        jwtVerificationKey: process.env.PRIVY_JWT_VERIFICATION_KEY,
+      },
+      { status: 500 }
+    );
   }
 }
