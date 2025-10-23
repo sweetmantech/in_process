14b7a271feac5342c6f128a4a6c704906a3ba1c9
thomas jin <tech.eng.me@gmail.com>
2025-10-24 03:33:04 +1000

    remove try-catch

--- hooks/useUsdcMint.ts
+++ hooks/useUsdcMint.ts
@@ -14,34 +14,29 @@ const useUsdcMint = () => {
     comment: string,
     mintCount: number = 1
   ) => {
-    try {
-      const usdcPrice = formatUnits(sale.pricePerToken, 6);
-      if (Number(balance) < Number(usdcPrice)) {
-        throw new Error(
-          "Insufficient balance. please topup USDC to your smart wallet. \n https://inprocess.fun/topup"
-        );
-      }
-      const response = await fetch("/api/moment/collect", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          token: {
-            tokenContractAddress: token.tokenContractAddress,
-            tokenId: token.tokenId,
-          },
-          account: artistWallet,
-          amount: mintCount,
-          comment,
-        }),
-      });
-      const data = await response.json();
-      return data.hash;
-    } catch (error) {
-      console.error(error);
-      return null;
+    const usdcPrice = formatUnits(sale.pricePerToken, 6);
+    if (Number(balance) < Number(usdcPrice)) {
+      throw new Error(
+        "Insufficient balance. please topup USDC to your smart wallet. \n https://inprocess.fun/topup"
+      );
     }
+    const response = await fetch("/api/moment/collect", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: {
+          tokenContractAddress: token.tokenContractAddress,
+          tokenId: token.tokenId,
+        },
+        account: artistWallet,
+        amount: mintCount,
+        comment,
+      }),
+    });
+    const data = await response.json();
+    return data.hash;
   };
 
   return {
