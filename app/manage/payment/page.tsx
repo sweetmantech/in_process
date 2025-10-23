67604d18da4c88e82373313f45ba144b803b6847
thomas jin <tech.eng.me@gmail.com>
2025-10-24 06:55:30 +1000

    make artistWallet to Address | undefined

--- app/manage/payment/page.tsx
+++ app/manage/payment/page.tsx
@@ -2,13 +2,12 @@
 
 import PaymentsTable from "@/components/PaymentsPage/PaymentsTable";
 import { useUserProvider } from "@/providers/UserProvider";
-import { Address } from "viem";
 
 const Accounts = () => {
   const { artistWallet } = useUserProvider();
   return (
     <main className="flex flex-col gap-24 font-archivo">
-      <PaymentsTable limit={50} address={artistWallet as Address} combined={true} />
+      <PaymentsTable limit={50} address={artistWallet} combined={true} />
     </main>
   );
 };
