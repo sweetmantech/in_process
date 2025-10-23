67013b0ddcfe44cfebaa5a7223a3f961616e63ce
thomas jin <tech.eng.me@gmail.com>
2025-10-24 06:49:30 +1000

    Update payment page to use artistWallet from UserProvider instead of signedAddress

--- app/manage/payment/page.tsx
+++ app/manage/payment/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import PaymentsTable from "@/components/PaymentsPage/PaymentsTable";
-import useSignedAddress from "@/hooks/useSignedAddress";
+import { useUserProvider } from "@/providers/UserProvider";
+import { Address } from "viem";
 
 const Accounts = () => {
-  const signedAddress = useSignedAddress();
+  const { artistWallet } = useUserProvider();
   return (
     <main className="flex flex-col gap-24 font-archivo">
-      <PaymentsTable limit={50} address={signedAddress} combined={true} />
+      <PaymentsTable limit={50} address={artistWallet as Address} combined={true} />
     </main>
   );
 };
