9df81411cea90e3bf416dc2490acbe3c430d01a0
kcethdev <tech.eng.me@gmail.com>
2025-11-13 00:17:46 -0500

    Add balance validation hook and approve call utility

---
+++ hooks/useCollectBalanceValidation.ts
@@ -0,0 +1,30 @@
+import { formatEther, formatUnits } from "viem";
+import { MintType } from "@/types/zora";
+import { SaleConfig } from "./useTokenInfo";
+import { useSmartWalletProvider } from "@/providers/SmartWalletProvider";
+import { showInsufficientBalanceError } from "@/lib/balance/showInsufficientBalanceError";
+
+const useCollectBalanceValidation = () => {
+  const { balance, ethBalance } = useSmartWalletProvider();
+
+  const validateBalance = (saleConfig: SaleConfig, mintCount: number = 1): void => {
+    const ethPrice = formatEther(saleConfig.pricePerToken * BigInt(mintCount));
+    const usdcPrice = formatUnits(saleConfig.pricePerToken * BigInt(mintCount), 6);
+
+    if (saleConfig.type === MintType.ZoraErc20Mint) {
+      if (Number(balance) < Number(usdcPrice)) {
+        showInsufficientBalanceError("usdc");
+      }
+    } else {
+      if (Number(ethBalance) < Number(ethPrice)) {
+        showInsufficientBalanceError("eth");
+      }
+    }
+  };
+
+  return {
+    validateBalance,
+  };
+};
+
+export default useCollectBalanceValidation;
