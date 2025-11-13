00c6fa5ca18d3c2c7507cffce6713e3ddc7987c6
kcethdev <tech.eng.me@gmail.com>
2025-11-13 00:17:23 -0500

    Add balance utilities for ETH and USDC

---
+++ lib/balance/getUsdcBalance.ts
@@ -0,0 +1,17 @@
+import { Address, erc20Abi, formatUnits } from "viem";
+import { getPublicClient } from "../viem/publicClient";
+import { CHAIN_ID, USDC_ADDRESS } from "../consts";
+
+const getUsdcBalance = async (address: Address): Promise<string> => {
+  const publicClient = getPublicClient(CHAIN_ID);
+  const balance = await publicClient.readContract({
+    address: USDC_ADDRESS,
+    abi: erc20Abi,
+    functionName: "balanceOf",
+    args: [address],
+  });
+
+  return formatUnits(balance, 6);
+};
+
+export default getUsdcBalance;
