217fb7439e574f506d5ff090fb2c08ff77a3b5e9
gm3 <tech.eng.me@gmail.com>
2025-11-18 11:08:27 +1000

    Refactor: Update layout files to use MomentCreateProviderWrapper

--- app/create/layout.tsx
+++ app/create/layout.tsx
@@ -1,14 +1,15 @@
 "use client";
+
 import CreateForm from "@/components/CreateForm";
 import CreateModeSelect from "@/components/CreateModeSelect";
 import MaskLines from "@/components/CreateModeSelect/MaskLines";
 import Moment from "@/components/Moment";
-import { MomentCreateProvider } from "@/providers/MomentCreateProvider";
+import MomentCreateProviderWrapper from "@/providers/MomentCreateProviderWrapper";
 import { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <MomentCreateProvider>
+    <MomentCreateProviderWrapper>
       <main className="w-screen grow">
         <div className="relative w-full mt-12 md:mt-24 grid grid-cols-1 md:grid-cols-3 gap-6 px-6 md:px-10">
           <MaskLines />
@@ -17,7 +18,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
           <CreateForm />
         </div>
       </main>
-    </MomentCreateProvider>
+    </MomentCreateProviderWrapper>
   );
 };
 
