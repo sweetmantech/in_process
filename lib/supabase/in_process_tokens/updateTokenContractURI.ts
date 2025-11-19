6cd4dde13dc01babd4c92e3f680e80d4b7bb373b
gm3 <tech.eng.me@gmail.com>
2025-11-19 20:25:06 +1000

    upsert contractURI

--- lib/supabase/in_process_tokens/updateTokenContractURI.ts
+++ lib/supabase/in_process_tokens/updateTokenContractURI.ts
@@ -6,6 +6,7 @@ interface UpdateTokenContractURIParams {
   tokenId: number;
   chainId: number;
   uri: string;
+  createdAt?: string;
 }
 
 export async function updateTokenContractURI({
@@ -13,13 +14,20 @@ export async function updateTokenContractURI({
   tokenId,
   chainId,
   uri,
+  createdAt,
 }: UpdateTokenContractURIParams) {
   const { data, error } = await supabase
     .from("in_process_tokens")
-    .update({ uri })
-    .eq("address", tokenContractAddress)
-    .eq("tokenId", tokenId)
-    .eq("chainId", chainId)
+    .upsert(
+      {
+        address: tokenContractAddress,
+        tokenId,
+        chainId,
+        uri,
+        createdAt: createdAt || new Date().toISOString(),
+      },
+      { onConflict: "address,chainId" }
+    )
     .select();
   return { data, error };
 }
