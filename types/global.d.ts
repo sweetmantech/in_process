ad8a9adceb4a600e6df34f1c242282722d341410
kcethdev <tech.eng.me@gmail.com>
2025-11-19 13:32:50 -0500

    disconnect after topup

--- types/global.d.ts
+++
@@ -1,8 +0,0 @@
-export {};
-
-declare global {
-  interface Window {
-    onNodeLabelChange: (nodeId: string, newLabel: string) => void;
-    onEdgeLabelChange: (edgeId: string, newLabel: string) => void;
-  }
-}
