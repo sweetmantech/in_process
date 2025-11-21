3c5ba5bcc04b9d4be0cabfd61f780bbc63979166
kcethdev <tech.eng.me@gmail.com>
2025-11-21 06:13:37 -0500

    fix feedback

--- providers/MomentCollectProvider.tsx
+++ providers/MomentCollectProvider.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import useMomentCollect from "@/hooks/useMomentCollect";
-import React, { createContext, useContext, useMemo } from "react";
+import React, { createContext, useContext } from "react";
 
 const MomentCollectContext = createContext<ReturnType<typeof useMomentCollect> | undefined>(
   undefined
 );
 
 const MomentCollectProvider = ({ children }: { children: React.ReactNode }) => {
-  const zoraMintComment = useMomentCollect();
+  const momentCollect = useMomentCollect();
 
-  const value = useMemo(() => ({ ...zoraMintComment }), [zoraMintComment]);
-
-  return <MomentCollectContext.Provider value={value}>{children}</MomentCollectContext.Provider>;
+  return (
+    <MomentCollectContext.Provider value={momentCollect}>{children}</MomentCollectContext.Provider>
+  );
 };
 
 const useMomentCollectProvider = () => {
