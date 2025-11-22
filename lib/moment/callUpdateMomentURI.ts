1a5b285db6879b84ed5ab25279846a19925602dd
kcethdev <tech.eng.me@gmail.com>
2025-11-22 07:43:41 -0500

    fix callUpdateMomentURI

--- lib/moment/callUpdateMomentURI.ts
+++ lib/moment/callUpdateMomentURI.ts
@@ -31,7 +31,11 @@ export async function callUpdateMomentURI({
       }),
     });
 
-    return await response.json();
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message || "Failed to update moment metadata");
+    }
+    return data.hash;
   } catch (error: any) {
     throw new Error(error?.message || "Failed to update moment metadata");
   }
