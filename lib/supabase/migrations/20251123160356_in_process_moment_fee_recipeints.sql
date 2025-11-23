b4f239995b38501ab1a1b535cd3151d3cf086d1b
kcethdev <tech.eng.me@gmail.com>
2025-11-23 11:15:47 -0500

    restricted

--- lib/supabase/migrations/20251123160356_in_process_moment_fee_recipeints.sql
+++ lib/supabase/migrations/20251123160356_in_process_moment_fee_recipeints.sql
@@ -18,7 +18,7 @@ CREATE UNIQUE INDEX in_process_moment_fee_recipients_pkey ON public.in_process_m
 
 alter table "public"."in_process_moment_fee_recipients" add constraint "in_process_moment_fee_recipients_pkey" PRIMARY KEY using index "in_process_moment_fee_recipients_pkey";
 
-alter table "public"."in_process_moment_fee_recipients" add constraint "in_process_moment_fee_recipients_artist_address_fkey" FOREIGN KEY (artist_address) REFERENCES public.in_process_artists(address) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+alter table "public"."in_process_moment_fee_recipients" add constraint "in_process_moment_fee_recipients_artist_address_fkey" FOREIGN KEY (artist_address) REFERENCES public.in_process_artists(address) ON UPDATE CASCADE ON DELETE RESTRICT not valid;
 
 alter table "public"."in_process_moment_fee_recipients" validate constraint "in_process_moment_fee_recipients_artist_address_fkey";
 
