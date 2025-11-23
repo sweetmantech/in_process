c6035aa1d2e90827c4b5fc1c0244602c091530cb
kcethdev <tech.eng.me@gmail.com>
2025-11-23 11:52:17 -0500

    update sales table

--- lib/supabase/migrations/20251123160356_in_process_moment_fee_recipeints.sql
+++ lib/supabase/migrations/20251123160356_in_process_moment_fee_recipeints.sql
@@ -39,3 +39,21 @@ alter table "public"."in_process_collections" validate constraint "in_process_co
 alter table "public"."in_process_moment_admins" add constraint "in_process_moment_admins_artist_address_fkey" FOREIGN KEY (artist_address) REFERENCES public.in_process_artists(address) ON UPDATE CASCADE ON DELETE RESTRICT not valid;
 
 alter table "public"."in_process_moment_admins" validate constraint "in_process_moment_admins_artist_address_fkey";
+
+alter table "public"."in_process_sales" drop constraint "in_process_sales_token_fkey";
+
+alter table "public"."in_process_sales" drop constraint "in_process_sales_token_unique";
+
+drop index if exists "public"."in_process_sales_token_unique";
+
+alter table "public"."in_process_sales" drop column "token";
+
+alter table "public"."in_process_sales" add column "moment" uuid not null;
+
+CREATE UNIQUE INDEX in_process_sales_token_unique ON public.in_process_sales USING btree (moment);
+
+alter table "public"."in_process_sales" add constraint "in_process_sales_moment_fkey" FOREIGN KEY (moment) REFERENCES public.in_process_moments(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+
+alter table "public"."in_process_sales" validate constraint "in_process_sales_moment_fkey";
+
+alter table "public"."in_process_sales" add constraint "in_process_sales_token_unique" UNIQUE using index "in_process_sales_token_unique";
