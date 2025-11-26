e3b89278dced3392c6585ce623e6f16b35a5c4d2
kcethdev <tech.eng.me@gmail.com>
2025-11-26 09:35:45 -0500

    fix total count

---
+++ lib/supabase/in_process_moments/getInProcessMomentsTotalCnt.ts
@@ -0,0 +1,16 @@
+import { supabase } from "@/lib/supabase/client";
+import { CHAIN_ID } from "@/lib/consts";
+
+export async function getInProcessMomentsTotalCnt(): Promise<{
+  count: number | null;
+  error: Error | null;
+}> {
+  const { count, error } = await supabase
+    .from("in_process_moments")
+    .select("*, collection:in_process_collections!inner(*)", { count: "exact", head: true })
+    .neq("collection.uri", "")
+    .eq("collection.chain_id", CHAIN_ID);
+
+  console.log("ziad here 1111", error);
+  return { count, error };
+}
