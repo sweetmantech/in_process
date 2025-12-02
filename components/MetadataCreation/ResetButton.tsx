70008c574b8617abdea8984ca13217fa7040b8df
gm3 <tech.eng.me@gmail.com>
2025-12-03 07:24:50 +1000

    fix build error

--- components/MetadataCreation/ResetButton.tsx
+++ components/MetadataCreation/ResetButton.tsx
@@ -2,16 +2,14 @@ import { Pencil1Icon } from "@radix-ui/react-icons";
 
 interface ResetButtonProps {
   onClick: () => void;
-  disabled: boolean;
 }
 
-const ResetButton = ({ onClick, disabled }: ResetButtonProps) => {
+const ResetButton = ({ onClick }: ResetButtonProps) => {
   return (
     <button
       type="button"
       className="absolute right-4 top-4 text-white bg-grey rounded-full p-2 z-[3] disabled:bg-gray-200 disabled:cursor-not-allowed"
       onClick={onClick}
-      disabled={disabled}
     >
       <Pencil1Icon className="size-4" />
     </button>
