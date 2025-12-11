107772ad84830fa3699237ba8b2a97788f074120
k.c <tech.eng.me@gmail.com>
2025-12-12 03:07:14 +1000

    add placeholder in airdrop input

--- components/MomentManagePage/AirdropInput.tsx
+++ components/MomentManagePage/AirdropInput.tsx
@@ -1,18 +1,21 @@
 import useAirdropInput from "@/hooks/useAirdropInput";
+import { useAirdropProvider } from "@/providers/AirdropProvider";
 
 const AirdropInput = () => {
   const { handleInput, handlePaste, handleBlur, value, setValue } = useAirdropInput();
+  const { airdopToItems } = useAirdropProvider();
 
   return (
     <input
       type="text"
-      className="h-fit px-2 py-1 font-archivo text-xs !outline-0 !ring-0 md:text-lg"
+      className="h-fit min-w-[300px] px-2 py-1 font-archivo text-xs !outline-0 !ring-0 md:text-lg"
       value={value}
       onChange={(e) => setValue(e.target.value)}
       onKeyDown={handleInput}
       onPaste={handlePaste}
       onBlur={handleBlur}
       autoFocus={true}
+      placeholder={airdopToItems.length > 0 ? "" : "Enter any wallet or ENS to airdrop."}
     />
   );
 };
