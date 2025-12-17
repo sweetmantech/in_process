d8fc50cc872bd828dc6132bf1f8fab3f09b9ff57
k.c <tech.eng.me@gmail.com>
2025-12-18 05:55:35 +1000

    fix art slider click event

--- components/ArtSlider/SliderFeed.tsx
+++ components/ArtSlider/SliderFeed.tsx
@@ -1,6 +1,6 @@
 import { useMetadata } from "@/hooks/useMetadata";
 import Loading from "../Loading";
-import { useRouter } from "next/navigation";
+import { useMomentNavigation } from "@/hooks/useMomentNavigation";
 import CarouselItem from "./CarouselItem";
 import { TimelineMoment } from "@/types/moment";
 
@@ -10,17 +10,12 @@ interface SliderFeedProps {
 
 const SliderFeed = ({ feed }: SliderFeedProps) => {
   const { data, isLoading } = useMetadata(feed.uri);
-  const { push } = useRouter();
-
-  const handleClick = () => {
-    push(`/${feed.default_admin.address}`);
-  };
-
+  const { handleMomentClick } = useMomentNavigation(feed);
   return (
     <button
       className="relative h-[200px] w-full overflow-hidden rounded-md bg-grey-moss-100 font-spectral md:h-auto"
       type="button"
-      onClick={handleClick}
+      onClick={handleMomentClick}
     >
       {isLoading || !data ? (
         <div className="flex size-full items-center justify-center rounded-md border border-grey bg-grey-moss-100">
