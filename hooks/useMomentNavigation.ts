9809c5d2e23854a97e13a08e6b9b06649758a66b
k.c <tech.eng.me@gmail.com>
2025-12-18 02:56:39 +1000

    add moment navigation

---
+++ hooks/useMomentNavigation.ts
@@ -0,0 +1,17 @@
+import { useRouter } from "next/navigation";
+import { TimelineMoment } from "@/types/moment";
+import { base } from "viem/chains";
+
+export const useMomentNavigation = (moment: TimelineMoment) => {
+  const { push } = useRouter();
+
+  const handleMomentClick = () => {
+    const { chain_id, address, token_id } = moment;
+    const shortName = chain_id === base.id ? "base" : "bsep";
+    if (shortName) {
+      push(`/collect/${shortName}:${address}/${token_id}`);
+    }
+  };
+
+  return { handleMomentClick };
+};
