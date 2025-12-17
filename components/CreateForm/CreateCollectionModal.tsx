f1b3d4e9e827f2ac553968b0d6c56733dcfe4a9e
k.c <tech.eng.me@gmail.com>
2025-12-18 05:25:22 +1000

    remove unused code

--- components/CreateForm/CreateCollectionModal.tsx
+++ components/CreateForm/CreateCollectionModal.tsx
@@ -3,7 +3,6 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import CollectionImage from "@/components/CollectionImage";
 import { useMetadataFormProvider } from "@/providers/MetadataFormProvider";
 import { useCreateCollectionProvider } from "@/providers/CollectionCreateProvider/CreateCollectionProvider";
