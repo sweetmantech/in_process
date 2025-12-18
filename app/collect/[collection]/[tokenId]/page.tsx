3c889d0ae98a483fc3f762c4aef5ae3db09c5c5e
k.c <tech.eng.me@gmail.com>
2025-12-19 07:42:05 +1000

    fix vercel og image

--- app/collect/[collection]/[tokenId]/page.tsx
+++ app/collect/[collection]/[tokenId]/page.tsx
@@ -1,8 +1,10 @@
 import MomentPage from "@/components/MomentPage";
-import fetchTokenMetadata from "@/lib/fetchTokenMetadata";
 import { APP_URL, VERCEL_OG } from "@/lib/og/consts";
 import { Metadata, NextPage } from "next";
 import { parseCollectionAddress } from "@/lib/timeline/parseCollectionAddress";
+import { CHAIN_ID } from "@/lib/consts";
+import { getMomentAdvancedInfo } from "@/lib/moment/getMomentAdvancedInfo";
+import { fetchTokenMetadata } from "@/lib/protocolSdk/ipfs/token-metadata";
 
 type Props = {
   params: Promise<{ collection: string; tokenId: string }>;
@@ -13,16 +15,24 @@ export const revalidate = 300;
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { tokenId, collection } = await params;
   const { address } = parseCollectionAddress(collection);
-  const metadata = await fetchTokenMetadata(collection as string, tokenId as string);
+  if (!address) throw new Error("Collection address is required");
+  if (!tokenId) throw new Error("Token ID is required");
+
+  const moment = {
+    collectionAddress: address.toLowerCase() as `0x${string}`,
+    tokenId,
+    chainId: CHAIN_ID,
+  };
+  const { uri } = await getMomentAdvancedInfo(moment);
+  if (!uri) throw Error("failed to get moment uri");
+  const metadata = await fetchTokenMetadata(uri);
 
   const title = metadata?.name || "In Process";
   const description = metadata?.description || "Imagined by LATASHÁ";
 
-  const addressParam = address || "";
-
   const frame = {
     version: "next",
-    imageUrl: `${VERCEL_OG}/api/og/token?collection=${addressParam}&tokenId=${tokenId}`,
+    imageUrl: `${VERCEL_OG}/api/og/moment?collectionAddress=${moment.collectionAddress}&tokenId=${moment.tokenId}`,
     aspectRatio: "3:2",
     button: {
       title: "Collect",
@@ -30,7 +40,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
         type: "launch_frame",
         name: "In Process",
         url: `${APP_URL}/collect/${collection}/${tokenId}`,
-        iconImageUrl: `${VERCEL_OG}/api/og/token?collection=${addressParam}&tokenId=${tokenId}`,
+        iconImageUrl: `${VERCEL_OG}/api/og/moment?collectionAddress=${moment.collectionAddress}&tokenId=${moment.tokenId}`,
         splashImageUrl: `${VERCEL_OG}/desktop_footer_logo.png`,
         splashBackgroundColor: "#e9ccbb",
       },
@@ -43,7 +53,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     openGraph: {
       title,
       description,
-      images: [`${VERCEL_OG}/api/og/token?collection=${addressParam}&tokenId=${tokenId}`],
+      images: [
+        `${VERCEL_OG}/api/og/moment?collectionAddress=${moment.collectionAddress}&tokenId=${moment.tokenId}`,
+      ],
     },
     other: {
       "fc:frame": JSON.stringify(frame),
