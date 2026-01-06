31e7b3336de7e93becaaf1f69eaf3418b34ed004
k.c <tech.eng.me@gmail.com>
2026-01-06 11:22:18 +1000

    fix feedback

--- app/api/sms/route.ts
+++ app/api/sms/route.ts
@@ -4,9 +4,7 @@ import getCorsHeader from "@/lib/getCorsHeader";
 import type { InboundMessageWebhookEvent } from "telnyx/resources/webhooks";
 import { updatePhoneVerified } from "@/lib/supabase/in_process_artist_phones/updatePhoneVerified";
 import { sendSms } from "@/lib/phones/sendSms";
-import selectPhone from "@/lib/supabase/in_process_artist_phones/selectPhone";
-import createMomentFromPhoto from "@/lib/phones/createMomentFromPhoto";
-import { IS_TESTNET, SITE_ORIGINAL_URL } from "@/lib/consts";
+import { processMmsPhoto } from "@/lib/phones/processMmsPhoto";
 
 const corsHeaders = getCorsHeader();
 
@@ -54,18 +52,7 @@ export async function POST(req: NextRequest) {
           );
         }
         if (type === "MMS" && media && media?.length > 0) {
-          const phone = await selectPhone(fromPhoneNumber);
-          if (!phone.verified) throw new Error("Phone number is not verified.");
-          const photo = media[0];
-          const { contractAddress, tokenId } = await createMomentFromPhoto(
-            photo,
-            event.data.payload,
-            phone.artist.address
-          );
-          await sendSms(
-            fromPhoneNumber,
-            `Moment created! ${SITE_ORIGINAL_URL}/collect/${IS_TESTNET ? "bsep" : "base"}:${contractAddress}/${tokenId}`
-          );
+          await processMmsPhoto(fromPhoneNumber, media[0], event.data.payload);
         }
       }
     }
