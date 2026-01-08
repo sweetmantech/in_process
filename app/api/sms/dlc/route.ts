4419528aab6b7d9a39ffc86a0b453c5ae47221b3
k.c <tech.eng.me@gmail.com>
2026-01-09 07:40:53 +1000

    Create route.ts

---
+++ app/api/sms/dlc/route.ts
@@ -0,0 +1,45 @@
+import getCorsHeader from "@/lib/getCorsHeader";
+import client from "@/lib/telnyx/client";
+
+const corsHeaders = getCorsHeader();
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 200,
+    headers: corsHeaders,
+  });
+}
+
+export async function GET() {
+  try {
+    const messageParams = {
+      to: "+12063566783",
+      text: "Hello, World!",
+      type: "SMS" as const,
+      subject: "10DLC test",
+    };
+
+    const response = await client.messages.send(messageParams);
+
+    const result = {
+      success: true,
+      message_id: response.data?.id,
+    };
+
+    return Response.json(result, { headers: corsHeaders });
+  } catch (e: any) {
+    console.error("Error in send message API:", e);
+    const message = e?.message ?? "Failed to send message";
+
+    const result = {
+      success: false,
+      error: message,
+    };
+
+    return Response.json(result, { status: 500, headers: corsHeaders });
+  }
+}
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
