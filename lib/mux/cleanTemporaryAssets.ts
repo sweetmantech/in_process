7e0df396cf41a891babad611f6e665ef32fd0c7d
k.c <tech.eng.me@gmail.com>
2026-01-13 23:59:00 +1000

    add comment

--- lib/mux/cleanTemporaryAssets.ts
+++ lib/mux/cleanTemporaryAssets.ts
@@ -5,6 +5,8 @@ const cleanTemporaryAssets = async () => {
   const currentEpoch = Date.now();
   try {
     for await (const asset of mux.video.assets.list()) {
+      // Delete temporary assets: if an asset was uploaded more than 1 hour ago and is still in Mux,
+      // it indicates the artist uploaded the asset but never completed the moment creation process.
       if (Number(asset.created_at) * 1000 > currentEpoch - 60 * 60 * 1000) {
         await deleteMuxAsset(asset.id);
       }
