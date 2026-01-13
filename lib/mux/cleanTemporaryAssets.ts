561e2e566fd2df32d04f2f5ee75e4ea38b6c3671
k.c <tech.eng.me@gmail.com>
2026-01-14 00:03:39 +1000

    fix feedback

--- lib/mux/cleanTemporaryAssets.ts
+++ lib/mux/cleanTemporaryAssets.ts
@@ -2,15 +2,19 @@ import mux from ".";
 import { deleteMuxAsset } from "./deleteAsset";
 
 const cleanTemporaryAssets = async () => {
+  const temporaryAssets = [];
   const currentEpoch = Date.now();
   try {
     for await (const asset of mux.video.assets.list()) {
       // Delete temporary assets: if an asset was uploaded more than 1 hour ago and is still in Mux,
       // it indicates the artist uploaded the asset but never completed the moment creation process.
-      if (Number(asset.created_at) * 1000 > currentEpoch - 60 * 60 * 1000) {
-        await deleteMuxAsset(asset.id);
-      }
+      if (Number(asset.created_at) * 1000 > currentEpoch - 60 * 60 * 1000)
+        temporaryAssets.push(asset);
     }
+    const promise = temporaryAssets.map(async (asset) => {
+      await deleteMuxAsset(asset.id);
+    });
+    await Promise.all(promise);
   } catch (assetsError: any) {
     console.error("Error fetching assets list:", assetsError);
   }
