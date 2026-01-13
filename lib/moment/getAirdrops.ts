6f922e17fd627f8fdf3d5aebea969cd042538343
k.c <tech.eng.me@gmail.com>
2026-01-14 02:30:16 +1000

    fix feedback

--- lib/moment/getAirdrops.ts
+++ lib/moment/getAirdrops.ts
@@ -1,26 +1,19 @@
 import { z } from "zod";
 import { getAirdropSchema } from "../schema/getAirdropSchema";
-import selectMoments from "../supabase/in_process_moments/selectMoments";
 import selectAirdrops from "../supabase/in_process_airdrops/selectAirdrops";
 
 export type GetAirdropsInput = z.infer<typeof getAirdropSchema>;
 
-export async function getAirdrops({ moment, offset }: GetAirdropsInput) {
-  const data = await selectMoments(moment);
-
-  const momentData = data[0];
-
-  if (!momentData) {
-    throw new Error("Moment not found");
-  }
-
+export async function getAirdrops({ artist_address, offset }: GetAirdropsInput) {
   const airdrops = await selectAirdrops({
-    momentId: momentData.id,
+    artist_address,
     offset,
   });
 
   const formattedAirdrops =
     airdrops?.map((airdrop) => ({
+      collectionAddress: airdrop.moment.collection.address,
+      tokenId: airdrop.moment.token_id,
       recipient: {
         address: airdrop.recipient.address,
         username: airdrop.recipient.username || null,
--- lib/supabase/in_process_airdrops/selectAirdrops.ts
+++ lib/supabase/in_process_airdrops/selectAirdrops.ts
@@ -1,14 +1,20 @@
 import { supabase } from "../client";
 
-const selectAirdrops = async ({ momentId, offset }: { momentId: string; offset: number }) => {
+const selectAirdrops = async ({
+  artist_address,
+  offset,
+}: {
+  artist_address: string;
+  offset: number;
+}) => {
   let query = supabase
     .from("in_process_airdrops")
-    .select("amount, recipient:in_process_artists!inner(address, username)")
+    .select(
+      "amount, recipient:in_process_artists!inner(address, username), moment:in_process_moments!inner(token_id, collection:in_process_collections!inner(default_admin, address))"
+    )
+    .eq("moment.collection.default_admin", artist_address.toLowerCase())
     .order("updated_at", { ascending: false });
 
-  if (momentId) {
-    query = query.eq("moment", momentId);
-  }
   query = query.range(offset, offset + 99); // Limit to 100 results per page
 
   const { data, error } = await query;
