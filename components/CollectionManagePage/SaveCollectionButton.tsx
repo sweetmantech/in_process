2a31fc36a71434d698fc1797f79d20bb359b73dc
k.c <tech.eng.me@gmail.com>
2026-01-15 01:18:57 +1000

    hover styling

--- components/CollectionManagePage/SaveCollectionButton.tsx
+++ components/CollectionManagePage/SaveCollectionButton.tsx
@@ -47,7 +47,7 @@ const SaveCollectionButton = ({ onSuccess }: SaveCollectionButtonProps) => {
 
   return (
     <button
-      className="w-fit rounded-md bg-black px-8 py-2 text-grey-eggshell disabled:opacity-50"
+      className="w-fit rounded-md bg-black px-8 py-2 text-grey-eggshell transition-colors hover:bg-grey-moss-300 disabled:opacity-50"
       onClick={handleSave}
       disabled={isSaving || !isOwner || !isFormValid}
     >
