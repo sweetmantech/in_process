0f60cf32599216f54c0e707a4c7ec33749f6b495
k.c <tech.eng.me@gmail.com>
2026-01-16 03:35:46 +1000

    fix note

--- components/SMSMomentPage/Notes.tsx
+++ components/SMSMomentPage/Notes.tsx
@@ -1,36 +1,20 @@
+import { Fragment } from "react";
 import { useUserProvider } from "@/providers/UserProvider";
 import { LoginButton } from "../LoginButton/LoginButton";
-import { InfoIcon } from "lucide-react";
 import { useMomentProvider } from "@/providers/MomentProvider";
+import Note from "../Note";
 
 const Notes = () => {
   const { artistWallet } = useUserProvider();
   const { isOwner } = useMomentProvider();
 
-  if (artistWallet && !isOwner)
-    return (
-      <div className="rounded-lg border border-grey-moss-200 bg-grey-moss-50 p-4 mb-4">
-        <div className="flex items-start gap-3">
-          <InfoIcon className="mt-0.5 h-5 w-5 flex-shrink-0 text-grey-moss-600" />
-          <p className="font-archivo text-sm text-grey-moss-900">
-            you are not an admin of this moment.
-          </p>
-        </div>
-      </div>
-    );
+  if (artistWallet && !isOwner) return <Note>you are not an admin of this moment.</Note>;
 
-  if (artistWallet) return null;
+  if (artistWallet) return <Fragment />;
 
   return (
     <>
-      <div className="rounded-lg border border-grey-moss-200 bg-grey-moss-50 p-4 mb-4">
-        <div className="flex items-start gap-3">
-          <InfoIcon className="mt-0.5 h-5 w-5 flex-shrink-0 text-grey-moss-600" />
-          <p className="font-archivo text-sm text-grey-moss-900">
-            sign in to airdrop and edit the description.
-          </p>
-        </div>
-      </div>
+      <Note>sign in to airdrop and edit the description.</Note>
       <LoginButton />
     </>
   );
