d0fc57a932b3f5a0b6ce28cc1a2b9c9ec1d5602c
k.c <tech.eng.me@gmail.com>
2026-01-19 20:26:56 +1000

    include admins

--- types/collections.ts
+++ types/collections.ts
@@ -36,6 +36,7 @@ export interface CollectionResponse {
     address: string;
     username: string | null;
   };
+  admins: Address[];
   payout_recipient: string;
   created_at: string;
   updated_at: string;
