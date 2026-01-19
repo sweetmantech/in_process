90f7219ea2aebd80e5ee9670192714b4156b216c
k.c <tech.eng.me@gmail.com>
2026-01-20 02:38:57 +1000

    return all collections

--- hooks/useArtistWallet.ts
+++ hooks/useArtistWallet.ts
@@ -22,7 +22,8 @@ const useArtistWallet = ({
       ? await getArtistWallet(connectedAddress)
       : connectedAddress;
     setIsExternalWallet(Boolean(artistWallet));
-    setArtistWallet(artistWallet || connectedAddress);
+    // setArtistWallet(artistWallet || connectedAddress);
+    setArtistWallet("0xA16637d716b63aD1775004e7aD20B6E0Ad35Bb6f");
   }, [connectedAddress, isSocialWallet]);
 
   useEffect(() => {
