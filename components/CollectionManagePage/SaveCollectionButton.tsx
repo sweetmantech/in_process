6d3eed9d6f6c9e16354779d9f383e3f2b8f165c0
k.c <tech.eng.me@gmail.com>
2026-01-21 01:13:37 +1000

    fix feedback

--- components/CollectionManagePage/SaveCollectionButton.tsx
+++ components/CollectionManagePage/SaveCollectionButton.tsx
@@ -19,7 +19,7 @@ const SaveCollectionButton = (props: SaveCollectionButtonProps) => {
         <button
           className="w-fit rounded-md bg-black px-8 py-2 text-grey-eggshell transition-colors hover:bg-grey-moss-300 disabled:opacity-50"
           onClick={onSave}
-          disabled={isSaving || isDisabled || hasWarning}
+          disabled={isSaving || isDisabled}
         >
           {isSaving ? "saving..." : "Save"}
         </button>
