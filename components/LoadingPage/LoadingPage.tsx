aadc607348d3f555cb1601afae11c71f1c13cced
k.c <tech.eng.me@gmail.com>
2026-01-29 12:56:51 +1000

    fix feedback

--- components/LoadingPage/LoadingPage.tsx
+++ components/LoadingPage/LoadingPage.tsx
@@ -3,7 +3,7 @@ import Loading from "../Loading";
 const LoadingPage = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <Loading className="aspect-[1/1] w-18" />
+      <Loading className="aspect-[1/1] w-16" />
     </div>
   );
 };
