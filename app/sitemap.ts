d969cb22e176944be38679a1c7931a5c711a3283
k.c <tech.eng.me@gmail.com>
2026-02-04 03:09:24 +1000

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,6 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://inprocess.world";
-
   return [
     {
       url: baseUrl,
