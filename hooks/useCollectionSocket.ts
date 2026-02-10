a16182b08a069f31dca55bb6ed87c9e23a26bd9b
k.c <tech.eng.me@gmail.com>
2026-02-11 04:31:35 +1000

    add dev console

--- hooks/useCollectionSocket.ts
+++ hooks/useCollectionSocket.ts
@@ -17,11 +17,13 @@ const useCollectionSocket = (collectionAddress: string, chainId: number) => {
 
     socket.on("collection:updated", (payload: CollectionUpdatedPayload) => {
       try {
+        console.log("ziad here");
         const addressMatch =
           getAddress(payload.collectionAddress) === getAddress(collectionAddress);
         const chainMatch = payload.chainId === chainId;
 
         if (addressMatch && chainMatch) {
+          console.log("invalidateQueries");
           queryClient.invalidateQueries({
             queryKey: ["collection", collectionAddress, chainId],
           });
