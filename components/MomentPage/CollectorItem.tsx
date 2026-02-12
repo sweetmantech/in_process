6f3af04022a05a76b4dfd4680fc3e2856856d5f9
k.c <tech.eng.me@gmail.com>
2026-02-13 06:33:34 +1000

    self collector

--- components/MomentPage/CollectorItem.tsx
+++ components/MomentPage/CollectorItem.tsx
@@ -2,12 +2,18 @@ import truncateAddress from "@/lib/truncateAddress";
 import { Collector } from "@/types/moment";
 import CopyIcon from "./CopyIcon";
 import { EXPLORER_URL } from "@/lib/consts";
+import { useUserProvider } from "@/providers/UserProvider";
 
 const CollectorItem = ({ collector, username, amount, transactionHash, timestamp }: Collector) => {
+  const { artistWallet } = useUserProvider();
+  const isYou = artistWallet?.toLowerCase() === collector.toLowerCase();
+
   return (
     <div className="px-1 md:px-2 space-y-0.5">
       <div className="flex items-center justify-between">
-        <p className="font-archivo text-sm font-medium">{username || truncateAddress(collector)}</p>
+        <p className="font-archivo text-sm font-medium">
+          {isYou ? "You" : username || truncateAddress(collector)}
+        </p>
         <p className="font-archivo text-sm">{amount}x</p>
       </div>
       <div className="flex flex-wrap items-center gap-x-1.5 gap-y-0.5 font-archivo text-[11px] text-neutral-400">
