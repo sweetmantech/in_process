b355c2f50c84b7718ae3909ecec8b824737c8778
ziad <tech.eng.me@gmail.com>
2026-04-15 03:31:03 +1000

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -15,5 +15,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/llms.txt`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
   ];
 }
